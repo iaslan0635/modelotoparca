31402bb263765e7e5fff6a71434edb0493c97566
iaslan <iaslan3506@gmail.com>
2023-04-05 09:28:22 +0300

    birkaç tasarım düzenlenmesi yapıldı

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,91 +1,81 @@
 <footer class="site__footer">
     <div class="site-footer">
-        <div class="decor site-footer__decor decor--type--bottom">
-            <div class="decor__body">
-                <div class="decor__start"></div>
-                <div class="decor__end"></div>
-                <div class="decor__center"></div>
-            </div>
-        </div>
         <div class="site-footer__widgets">
             <div class="container">
                 <div class="row">
                     <div class="col-12 col-xl-4">
                         <div class="site-footer__widget footer-contacts">
-                            <h5 class="footer-contacts__title">Contact Us</h5>
+                            <h5 class="footer-contacts__title">Bize Ulaşın</h5>
                             <div class="footer-contacts__text">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing
                                 elit. Integer in feugiat lorem.
                             </div>
                             <address class="footer-contacts__contacts">
                                 <dl>
-                                    <dt>Phone Number</dt>
-                                    <dd>+1 (800) 060-07-30</dd>
+                                    <dt>Telefon</dt>
+                                    <dd>+1 (800) 000-00-00</dd>
                                 </dl>
                                 <dl>
                                     <dt>Email Address</dt>
-                                    <dd>red-parts@example.com</dd>
+                                    <dd>info@modelotoparca.com</dd>
                                 </dl>
                                 <dl>
-                                    <dt>Our Location</dt>
-                                    <dd>715 Fake Street, New York 10021 USA</dd>
+                                    <dt>Şube</dt>
+                                    <dd>Merkez/Niğde</dd>
                                 </dl>
                                 <dl>
-                                    <dt>Working Hours</dt>
-                                    <dd>Mon-Sat 10:00pm - 7:00pm</dd>
+                                    <dt>Şube-2</dt>
+                                    <dd>Kayseri</dd>
                                 </dl>
                             </address>
                         </div>
                     </div>
                     <div class="col-6 col-md-3 col-xl-2">
                         <div class="site-footer__widget footer-links">
-                            <h5 class="footer-links__title">Information</h5>
+                            <h5 class="footer-links__title">Hakkımızda</h5>
                             <ul class="footer-links__list">
-                                <li class="footer-links__item"><a href="" class="footer-links__link">About Us</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Delivery Information</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Privacy Policy</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Brands</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Contact Us</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Returns</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Site Map</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Hakkımızda</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Hizmet Şartları</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Gizlilik Koşulları</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Sipariş Takibi</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Bilgiler</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Site Haritası</a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-6 col-md-3 col-xl-2">
                         <div class="site-footer__widget footer-links">
-                            <h5 class="footer-links__title">My Account</h5>
+                            <h5 class="footer-links__title">Kullanıcı</h5>
                             <ul class="footer-links__list">
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Store Location</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Order History</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Wish List</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Newsletter</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Specials</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Gift Certificates</a></li>
-                                <li class="footer-links__item"><a href="" class="footer-links__link">Affiliate</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Müşteri Paneli</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Garajım</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Sipariş Geçmişi</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">Beğenilen Ürünler</a></li>
+                                <li class="footer-links__item"><a href="" class="footer-links__link">İletişim</a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-12 col-md-6 col-xl-4">
                         <div class="site-footer__widget footer-newsletter">
-                            <h5 class="footer-newsletter__title">Newsletter</h5>
+                            <h5 class="footer-newsletter__title">Haber Bülteni</h5>
                             <div class="footer-newsletter__text">
-                                Enter your email address below to subscribe to our newsletter and keep up to date with discounts and special offers.
+                                Duyuru ve kampanyalarımızdan faydalanmak için, Haber bültenine kayıt olunuz
                             </div>
                             <form action="" class="footer-newsletter__form">
                                 <label class="sr-only" for="footer-newsletter-address">Email Address</label>
                                 <input type="text" class="footer-newsletter__form-input" id="footer-newsletter-address" placeholder="Email Address...">
-                                <button class="footer-newsletter__form-button">Subscribe</button>
+                                <button class="footer-newsletter__form-button">Ekle</button>
                             </form>
                             <div class="footer-newsletter__text footer-newsletter__text--social">
-                                Follow us on social networks
+                                Sosyal Medyada Biz
                             </div>
                             <div class="footer-newsletter__social-links social-links">
                                 <ul class="social-links__list">
-                                    <li class="social-links__item social-links__item--facebook"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li class="social-links__item social-links__item--twitter"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                                    <li class="social-links__item social-links__item--youtube"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fab fa-youtube"></i></a></li>
-                                    <li class="social-links__item social-links__item--instagram"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                                    <li class="social-links__item social-links__item--rss"><a href="https://themeforest.net/user/kos9" target="_blank"><i class="fas fa-rss"></i></a></li>
+                                    <li class="social-links__item social-links__item--facebook"><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li class="social-links__item social-links__item--twitter"><a href="#"  target="_blank"><i class="fab fa-twitter"></i></a></li>
+                                    <li class="social-links__item social-links__item--youtube"><a href="#"  target="_blank"><i class="fab fa-youtube"></i></a></li>
+                                    <li class="social-links__item social-links__item--instagram"><a href="#"  target="_blank"><i class="fab fa-instagram"></i></a></li>
+                                    <li class="social-links__item social-links__item--rss"><a href="#"  target="_blank"><i class="fas fa-rss"></i></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -98,7 +88,7 @@
                 <div class="site-footer__bottom-row">
                     <div class="site-footer__copyright">
                         <!-- copyright -->
-                        Powered by HTML — Designed by <a href="https://themeforest.net/user/kos9" target="_blank">Kos</a>
+                        Tüm Hakları Saklıdır | <a href="#" target="_blank"> Modelotoparca</a>
                         <!-- copyright / end -->
                     </div>
                     <div class="site-footer__payments">
