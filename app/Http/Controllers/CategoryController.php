e06ef2490c9eb087d9fe6b9458f0c3db751009d2
iaslan <iaslan3506@gmail.com>
2023-04-07 17:24:26 +0300

    category page create

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        //
+       return view('categories');
     }
 
     public function show(Category $category)
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('search', function () { // http://127.0.0.1:8000/search?q=MAHLEOX422D
 Route::view('product-list', 'product-list')->name('product-list');
 Route::view('models-list', 'models-list')->name('models-list');
 
-
+Route::get('categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('category.index');
 Route::get('c/{category:slug}', [\App\Http\Controllers\CategoryController::class, 'show'])->name('category.show');
 Route::prefix('p/{product:slug}')->group(function () {
     Route::get('quickview', [\App\Http\Controllers\ProductController::class, 'quickview'])->name('quickview');
