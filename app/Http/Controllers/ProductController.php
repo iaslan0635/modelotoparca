eaf7861195a0a116b2642757bcc45fd27163f824
turgutefeyuksel <turgutefeyuksel@gmail.com>
2023-04-15 05:25:31 +0300

    Latest Products added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Models\ProductOem;
+use App\Packages\LatestProducts;
 
 class ProductController extends Controller
 {
@@ -15,8 +16,8 @@ class ProductController extends Controller
         $oems = $product->oems()->groupBy('brand')->selectRaw("brand, GROUP_CONCAT(oem) as oems")->get();
 //            ->map(fn(ProductOem $oem) => ["brand" => $oem->brand, "oems" => explode(",", $oem->oem)]);
 
-        $car_brands = $product->cars()->with("maker:id,name")->get()->groupBy("maker.name")->map(fn ($a) => $a->groupBy("short_name"));
-
+        $car_brands = $product->cars()->with("maker:id,name")->get()->groupBy("maker.name")->map(fn($a) => $a->groupBy("short_name"));
+        LatestProducts::add($product);
         return view('product-detail', compact('product', 'oems', 'car_brands'));
     }
 
@@ -24,6 +25,7 @@ class ProductController extends Controller
     {
         $product->load('brand');
         $product->visit()->withIP()->withUser();
+        LatestProducts::add($product);
         return view('quickview', compact('product'));
     }
 }
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -865,120 +865,38 @@
                                                 </div>
                                             </div>
                                             <div class="widget-filters__actions d-flex">
-                                                <button type="submit" class="btn btn-primary btn-sm">Filter</button>
-                                                <button class="btn btn-secondary btn-sm">Reset</button>
+                                                <button type="submit" class="btn btn-primary btn-sm">Filtrele</button>
+                                                <button type="reset" class="btn btn-secondary btn-sm">Sıfırla</button>
                                             </div>
                                         </div>
                                     </form>
                                     <div class="card widget widget-products d-none d-lg-block">
                                         <div class="widget__header">
-                                            <h4>Latest Products</h4>
+                                            <h4>Son Ziyaret Ettiğim Ürünler</h4>
                                         </div>
                                         <div class="widget-products__list">
-                                            <div class="widget-products__item">
-                                                <div class="widget-products__image image image--type--product">
-                                                    <a href="product-full.html" class="image__body">
-                                                        <img loading="lazy" class="image__tag"
-                                                             src="https://web.modelotoparca.com/images/products/product-1-64x64.jpg"
-                                                             alt="">
-                                                    </a>
-                                                </div>
-                                                <div class="widget-products__info">
-                                                    <div class="widget-products__name">
-                                                        <a href="product-full.html">Brandix Spark Plug Kit ASR-400</a>
+                                            @foreach(\App\Packages\LatestProducts::items() as $item)
+                                                <div class="widget-products__item">
+                                                    <div class="widget-products__image image image--type--product">
+                                                        <a href="{{ route('product.show', $item) }}" class="image__body">
+                                                            <img loading="lazy" class="image__tag"
+                                                                 src="{{ $item->imageUrl() }}"
+                                                                 alt="">
+                                                        </a>
                                                     </div>
-                                                    <div class="widget-products__prices">
-                                                        <div
-                                                            class="widget-products__price widget-products__price--current">
-                                                            $19.00
+                                                    <div class="widget-products__info">
+                                                        <div class="widget-products__name">
+                                                            <a href="{{ route('product.show', $item) }}">{{ $item->fullTitle }}</a>
+                                                        </div>
+                                                        <div class="widget-products__prices">
+                                                            <div
+                                                                class="widget-products__price widget-products__price--current">
+                                                                {{ $item->price->formattedPrice }}
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="widget-products__item">
-                                                <div class="widget-products__image image image--type--product">
-                                                    <a href="product-full.html" class="image__body">
-                                                        <img loading="lazy" class="image__tag"
-                                                             src="https://web.modelotoparca.com/images/products/product-2-64x64.jpg"
-                                                             alt="">
-                                                    </a>
-                                                </div>
-                                                <div class="widget-products__info">
-                                                    <div class="widget-products__name">
-                                                        <a href="product-full.html">Brandix Brake Kit BDX-750Z370-S</a>
-                                                    </div>
-                                                    <div class="widget-products__prices">
-                                                        <div
-                                                            class="widget-products__price widget-products__price--current">
-                                                            $224.00
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="widget-products__item">
-                                                <div class="widget-products__image image image--type--product">
-                                                    <a href="product-full.html" class="image__body">
-                                                        <img loading="lazy" class="image__tag"
-                                                             src="https://web.modelotoparca.com/images/products/product-3-64x64.jpg"
-                                                             alt="">
-                                                    </a>
-                                                </div>
-                                                <div class="widget-products__info">
-                                                    <div class="widget-products__name">
-                                                        <a href="product-full.html">Left Headlight Of Brandix Z54</a>
-                                                    </div>
-                                                    <div class="widget-products__prices">
-                                                        <div class="widget-products__price widget-products__price--new">
-                                                            $349.00
-                                                        </div>
-                                                        <div class="widget-products__price widget-products__price--old">
-                                                            $415.00
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="widget-products__item">
-                                                <div class="widget-products__image image image--type--product">
-                                                    <a href="product-full.html" class="image__body">
-                                                        <img loading="lazy" class="image__tag"
-                                                             src="https://web.modelotoparca.com/images/products/product-4-64x64.jpg"
-                                                             alt="">
-                                                    </a>
-                                                </div>
-                                                <div class="widget-products__info">
-                                                    <div class="widget-products__name">
-                                                        <a href="product-full.html">Glossy Gray 19" Aluminium Wheel
-                                                            AR-19</a>
-                                                    </div>
-                                                    <div class="widget-products__prices">
-                                                        <div
-                                                            class="widget-products__price widget-products__price--current">
-                                                            $589.00
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="widget-products__item">
-                                                <div class="widget-products__image image image--type--product">
-                                                    <a href="product-full.html" class="image__body">
-                                                        <img loading="lazy" class="image__tag"
-                                                             src="https://web.modelotoparca.com/images/products/product-5-64x64.jpg"
-                                                             alt="">
-                                                    </a>
-                                                </div>
-                                                <div class="widget-products__info">
-                                                    <div class="widget-products__name">
-                                                        <a href="product-full.html">Twin Exhaust Pipe From Brandix
-                                                            Z54</a>
-                                                    </div>
-                                                    <div class="widget-products__prices">
-                                                        <div
-                                                            class="widget-products__price widget-products__price--current">
-                                                            $749.00
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                            @endforeach
                                         </div>
                                     </div>
                                 </div>
