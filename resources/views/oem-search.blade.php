052b6762210e7e83734b91a356366b543b313ed9
iaslan <iaslan3506@gmail.com>
2023-05-02 19:31:19 +0300

    oem results page crate

--- resources/views/oem-search.blade.php
+++ resources/views/oem-search.blade.php
@@ -1044,9 +1044,9 @@
                                                                             d="M12,4.4L5.5,11L1,6.5l1.4-1.4l3.1,3.1L10.6,3L12,4.4z"/>
                                                                     </svg>
                                                                 </div>
-                                                                <div class="status-badge__text">Part Fit for 2011 Ford
-                                                                    Focus S
-                                                                </div>
+{{--                                                                <div class="status-badge__text">Part Fit for 2011 Ford--}}
+{{--                                                                    Focus S--}}
+{{--                                                                </div>--}}
                                                                 <div class="status-badge__tooltip" tabindex="0"
                                                                      data-toggle="tooltip"
                                                                      title="Part&#x20;Fit&#x20;for&#x20;2011&#x20;Ford&#x20;Focus&#x20;S"></div>
