9bb1f09d904ea05aa0389142a42d73edd989fa96
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-05-08 13:42:13 +0300

    import fix

--- app/Importers/TigerImporter.php
+++ app/Importers/TigerImporter.php
@@ -95,12 +95,12 @@ class TigerImporter extends Importer
 
                 $oems = $this->explode($productData["oem_codes"]) ?? [];
                 foreach ($oems as $oem)
-                    ProductOem::query()->updateOrInsert(["logicalref" => $productId], ["oem" => $oem, "brand" => ""]);
+                    ProductOem::query()->updateOrInsert(["logicalref" => $productId, "oem" => $oem, "brand" => ""]);
 
                 $priceProductId = $this->pop($priceData, "product_id");
                 Price::query()->updateOrInsert(["product_id" => $priceProductId], $priceData);
 
-                Category::query()->insertOrIgnore([
+                Category::query()->updateOrInsert([
                     "product_id" => $productId,
                     "category_id" => $categoryId
                 ]);
