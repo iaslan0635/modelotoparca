33d90ea6dd0d9abe34452c1da3574999398a1d4d
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-05-15 11:30:44 +0300

    spareto cache

--- app/Facades/SparetoCache.php
+++ app/Facades/SparetoCache.php
@@ -2,39 +2,29 @@
 
 namespace App\Facades;
 
+use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class SparetoCache
 {
-    public static function table()
+    private static $__driver;
+
+    public static function driver(): FilesystemAdapter
     {
-        return DB::table('spareto_cache');
+        return self::$__driver ?? (self::$__driver = \Storage::createLocalDriver(["root" => storage_path("spareto_cache")]));
     }
 
     /**
      * GET url using cache
      *
      * @param $url string
-     * @param  bool  $incrementHit
-     *
-     * @throws RequestException
+     * @return string
      */
-    public static function get(string $url, $incrementHit = true): string
+    public static function get(string $url): string
     {
-        $content = self::table()->where('url', $url)->value('content');
-
-        if (is_null($content)) {
-            $content = HTTP::get($url)->throw()->body();
-            self::table()->insertOrIgnore(compact('url', 'content'));
-        }
-
-        if ($incrementHit) {
-            self::table()->where('url', $url)->increment('hits');
-        }
-
-        return $content;
+        return self::has($url) ? self::readCache($url) : self::createCache($url);
     }
 
     /** Add url content to cache if not exists
@@ -44,12 +34,37 @@ class SparetoCache
      */
     public static function insert(string $url): void
     {
-        // Do nothing if cache exists
-        if (self::table()->where('url', $url)->exists()) {
-            return;
-        }
+        if (!self::has($url))
+            self::createCache($url);
+    }
 
-        $content = HTTP::get($url)->throw()->body();
-        self::table()->insertOrIgnore(compact('url', 'content'));
+    public static function has(string $url)
+    {
+        return self::driver()->has(self::getCacheFilePath($url));
+    }
+
+    protected static function request(string $url)
+    {
+        return HTTP::get($url)->throw()->body();
+    }
+
+    protected static function getCacheFilePath(string $url)
+    {
+        return hash("sha256", $url) . ".html";
+    }
+
+    /** @return string cache content */
+    protected static function createCache(string $url)
+    {
+        $path = self::getCacheFilePath($url);
+        $content = self::request($url);
+        self::driver()->write($path, $content);
+        return $content;
+    }
+
+    protected static function readCache(string $url)
+    {
+        $path = self::getCacheFilePath($url);
+        return self::driver()->get($path);
     }
 }
