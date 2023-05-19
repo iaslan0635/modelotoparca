ee9f738739c046a29b6cfd83f0860b09b90193fa
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-05-19 18:54:26 +0300

    SparetoConnectionResetJob

---
+++ app/Console/Commands/SparetoConnectionResetJob.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\SparetoConnection;
+use Illuminate\Console\Command;
+
+class SparetoConnectionResetJob extends Command
+{
+    protected $signature = 'spareto:connect:reset';
+
+    public function handle()
+    {
+        SparetoConnection::where("is_connection_applied", false)->update(["is_connection_applied" => true]);
+    }
+}
