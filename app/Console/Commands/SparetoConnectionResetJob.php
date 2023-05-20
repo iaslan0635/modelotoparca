8087d9af13a59ef132e76aadfc125dbe8dc97772
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-05-20 09:47:19 +0300

    add connection ids

--- app/Console/Commands/SparetoConnectionResetJob.php
+++ app/Console/Commands/SparetoConnectionResetJob.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\ProductOem;
 use App\Models\SparetoConnection;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class SparetoConnectionResetJob extends Command
 {
@@ -12,5 +14,7 @@ class SparetoConnectionResetJob extends Command
     public function handle()
     {
         SparetoConnection::where("is_connection_applied", true)->update(["is_connection_applied" => false]);
+        ProductOem::truncate();
+        DB::table("product_cars")->truncate();
     }
 }
