336216042e91f417ca6bd43cd8ad949459c1e116
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-05-26 16:17:04 +0300

    test commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-cd /home/ploi/site.modelotoparca.com || exit
-git pull origin main
-php artisan cache:clear
 
+if [ "$(git pull)" != "Already up to date." ]; then
+    cd /home/ploi/site.modelotoparca.com || exit
+    git pull origin main
+    php artisan cache:clear
+    echo "Deployed 🚀"
+fi
