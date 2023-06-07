49210113ac8f3f0bcad5d6f56fbe0a5486f30e1b
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-06-07 14:43:03 +0300

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+cd /home/ploi/site.modelotoparca.com || exit
 if [ "$(git pull)" != "Already up to date." ]; then
-    cd /home/ploi/site.modelotoparca.com || exit
     git pull origin main
     php artisan cache:clear
     echo "Deployed 🚀"
