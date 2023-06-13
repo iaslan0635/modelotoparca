b7d8815b06f984457383877d1e3f9e3f7b1789e5
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-06-13 17:09:29 +0300

    revert test

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -52,14 +52,14 @@ class Product extends BaseModel implements CanVisit
                 'producercode_regex' => $this->producercode ? strtolower(preg_replace('/[^a-zA-Z0-9]+/', '', $this->producercode)) : null,
                 'producercode2' => $this->producercode2,
                 'producercode2_regex' => $this->producercode2 ? strtolower(preg_replace('/[^a-zA-Z0-9]+/', '', $this->producercode2)) : null,
-            ] + [
+
                 'oems' => $this->oems->map->toSearchableArray(),
                 'cars' => $cars->map->toSearchableArray(),
                 'similars' => $this->similars->map->toSearchableArrayWithOnlyCrossCode(),
                 'categories' => $this->categories->map->toSearchableArray(),
                 'brand' => $this->brand?->toSearchableArray(),
                 'price' => $this->price?->price,
-            ] + [
+
                 'full_text' => collect([$this->title, $this->sub_title])->merge($cars->map->getRegexedName())->join(" | "),
             ];
     }
