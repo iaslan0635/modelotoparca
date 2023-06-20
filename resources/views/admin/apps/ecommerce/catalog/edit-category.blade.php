b3d542ba8c335bc2df89406991a4170d4359e80c
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-06-20 13:25:24 +0300

    .

--- resources/views/admin/apps/ecommerce/catalog/edit-category.blade.php
+++ resources/views/admin/apps/ecommerce/catalog/edit-category.blade.php
@@ -252,18 +252,6 @@
                             <!--end::Tab pane-->
                         </div>
                         <!--end::Tab content-->
-                        <div class="d-flex justify-content-end">
-                            <!--begin::Button-->
-                            <a href="admin/apps/ecommerce/catalog/categories" id="kt_ecommerce_add_product_cancel" class="btn btn-light me-5">Cancel</a>
-                            <!--end::Button-->
-                            <!--begin::Button-->
-                            <button type="submit" id="kt_ecommerce_add_product_submit" class="btn btn-primary">
-                                <span class="indicator-label">Save Changes</span>
-                                <span class="indicator-progress">Please wait...
-							<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                            </button>
-                            <!--end::Button-->
-                        </div>
                     </div>
                     <!--end::Main column-->
                 </div>
