c949fa9bee4a7ec24aac088cd4ba6403ab376328
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-07-11 15:06:20 +0300

    imageUrl fix

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $categories = Category::root()->limit(15)->orderBy("order")->get(["slug", "name", "id"]);
-    $featured_products = Product::query()->limit(20)->with("price:id,price,currency,product_id")->get(["id", "slug", "sku", "title"]);
+    $featured_products = Product::query()->limit(20)->with("price:id,price,currency,product_id")->get(["id", "slug", "sku", "title", "image_appendix"]);
     return view('home', compact('categories', 'featured_products'));
 })->name('welcome');
 
