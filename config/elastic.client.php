167365ef44a80196c7eb662aaf54b37cca04beb9
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-07-22 12:27:05 +0300

    elastic creds

--- config/elastic.client.php
+++ config/elastic.client.php
@@ -9,6 +9,10 @@ return [
             'hosts' => [
                 env('ELASTIC_HOST', 'localhost:9200'),
             ],
+            'basicAuthentication' => [
+                env('ELASTIC_USERNAME'),
+                env('ELASTIC_PASSWORD'),
+            ],
         ],
     ],
 ];
