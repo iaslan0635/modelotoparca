c1a78ac5c0908369a0ceeab158dfae79b3413506
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-07-25 15:18:02 +0300

    search replacement unique

---
+++ database/migrations/2023_07_25_151402_add_unique_to_search_replacements.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('search_replacements', function (Blueprint $table) {
+            $table->dropUnique("search_replacements_original_replacement_unique");
+            $table->unique("original");
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('search_replacements', function (Blueprint $table) {
+            $table->dropUnique("search_replacements_original_unique");
+            $table->unique(["original", "replacement"]);
+        });
+    }
+};
