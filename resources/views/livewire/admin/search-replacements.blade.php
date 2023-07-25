179c59dd531ba4ca594cb0ce3ea54fd65de7a4bc
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-07-25 18:23:19 +0300

    search replacements fix

--- resources/views/livewire/admin/search-replacements.blade.php
+++ resources/views/livewire/admin/search-replacements.blade.php
@@ -51,7 +51,7 @@
                 <!--end::Table head-->
                 <!--begin::Table body-->
                 <tbody class="fw-semibold text-gray-600">
-                @foreach(array_reverse($searchReplacements) as $key => $replacement)
+                @foreach(array_reverse($searchReplacements, true) as $key => $replacement)
                     <!--begin::Table row-->
                     <tr>
                         <td>
