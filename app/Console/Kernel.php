572231822e16aeb1901a4200d8e6b1bc24048191
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-08-28 10:00:45 +0300

    SparetoBot

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('queue:prune-batches')->daily();
     }
 
     /**
