e500dcf64b678418d2ea1710e3638ff71052610b
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-09-20 11:59:17 +0300

    Changes

--- resources/views/livewire/spareto-connection-ban-panel.blade.php
+++ resources/views/livewire/spareto-connection-ban-panel.blade.php
@@ -5,12 +5,12 @@
         @if($url->is_banned)
             <button class="btn btn-success" wire:click="unban" wire:loading.attr="disabled">
                 <span wire:loading class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                Yasağı kaldır
+                <span wire:loading.remove>Yasağı kaldır</span>
             </button>
         @else
             <button class="btn btn-danger" wire:click="ban" wire:loading.attr="disabled">
                 <span wire:loading class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                Yasakla
+                <span wire:loading.remove>Yasakla</span>
             </button>
         @endif
     </td>
