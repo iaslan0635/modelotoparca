aaeb560678f3a5222db126b7d5438612022b126f
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-09-20 12:06:48 +0300

    Changes

--- resources/views/livewire/spareto-connection-ban-panel.blade.php
+++ resources/views/livewire/spareto-connection-ban-panel.blade.php
@@ -6,9 +6,9 @@
     </td>
     <td>
         @if($url->is_banned)
-            <span class="badge rounded-pill text-white text-bg-success">Hayır</span>
+            <span class="badge rounded-pill text-white text-bg-success">Evet</span>
         @else
-            <span class="badge rounded-pill text-white text-bg-danger">Evet</span>
+            <span class="badge rounded-pill text-white text-bg-danger">Hayır</span>
         @endif
     </td>
     <td>
