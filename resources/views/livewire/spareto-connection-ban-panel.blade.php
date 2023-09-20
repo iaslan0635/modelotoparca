1f2e0e786e2a49cb330cd95e0365ff6e38616157
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-09-20 12:07:32 +0300

    Changes

--- resources/views/livewire/spareto-connection-ban-panel.blade.php
+++ resources/views/livewire/spareto-connection-ban-panel.blade.php
@@ -13,12 +13,12 @@
     </td>
     <td>
         @if($url->is_banned)
-            <button class="btn btn-success" wire:click="unban" wire:loading.attr="disabled">
+            <button class="btn btn-sm btn-success" wire:click="unban" wire:loading.attr="disabled">
                 <span wire:loading class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                 <span wire:loading.remove>Yasağı kaldır</span>
             </button>
         @else
-            <button class="btn btn-danger" wire:click="ban" wire:loading.attr="disabled">
+            <button class="btn btn-sm btn-danger" wire:click="ban" wire:loading.attr="disabled">
                 <span wire:loading class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                 <span wire:loading.remove>Yasakla</span>
             </button>
