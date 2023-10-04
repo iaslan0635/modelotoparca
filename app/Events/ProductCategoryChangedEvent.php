1dd625e781cb494a519b3f2425b0b16e00872047
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-10-04 10:27:04 +0300

    Added note

--- app/Events/ProductCategoryChangedEvent.php
+++ app/Events/ProductCategoryChangedEvent.php
@@ -5,6 +5,7 @@ namespace App\Events;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
+/** FIXME: Product category changes will not be dispatched because pivot tables have no events. */
 class ProductCategoryChangedEvent
 {
     use Dispatchable, SerializesModels;
