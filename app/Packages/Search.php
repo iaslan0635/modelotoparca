c1165f6effacf7d209b122e2c7aa39a46b8dd91e
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-10-13 17:48:02 +0300

    Revert "."
    This reverts commit 4ae2f1ca78ef98e3a0aca10bb8edb68b3615a49d.

--- app/Packages/Search.php
+++ app/Packages/Search.php
@@ -194,8 +194,8 @@ class Search
     private static function suggestionsProducerCode(string $term)
     {
         $suggestQuery = Query::bool()
-            ->should(Query::matchPhrasePrefix()->field('producercode')->query($term))
-            ->should(Query::matchPhrasePrefix()->field('producercode_regex')->query($term));
+            ->should(Query::prefix()->field('producercode')->value($term)->caseInsensitive(true))
+            ->should(Query::prefix()->field('producercode_regex')->value($term)->caseInsensitive(true));
 
         $suggestion = Product::searchQuery($suggestQuery)->highlight('producercode', [
             'pre_tags' => ['<strong>'],
