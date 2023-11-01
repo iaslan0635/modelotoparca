2b75ae0485b28ff5cbb8869a1dc60a5cef7364ba
turgutefeyuksel <turgutefeyuksel@gmail.com>
2023-11-01 23:46:55 +0300

    Livewire dirty added

--- resources/views/livewire/admin/product/create-merchant.blade.php
+++ resources/views/livewire/admin/product/create-merchant.blade.php
@@ -2,7 +2,7 @@
 <div class="d-flex align-items-center gap-2 gap-lg-3">
     @if(!$product->merchants()->where('merchant', 'n11')->first())
         <!--begin::Primary button-->
-        <button wire:click="createN11" class="btn btn-sm fw-bold btn-primary">
+        <button wire:dirty.attr="disabled" wire:click="createN11" class="btn btn-sm fw-bold btn-primary">
             N11 oluştur
         </button>
         <!--end::Primary button-->
