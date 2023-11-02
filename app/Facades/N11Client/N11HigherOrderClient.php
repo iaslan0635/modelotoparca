0e8a4da4a2e8cbe9fe42b947add703486ed6510a
turgutefeyuksel <turgutefeyuksel@gmail.com>
2023-11-02 03:25:20 +0300

    N11 changes

--- app/Facades/N11Client/N11HigherOrderClient.php
+++ app/Facades/N11Client/N11HigherOrderClient.php
@@ -11,7 +11,7 @@ class N11HigherOrderClient
     public function __call(string $name, $arguments)
     {
         $data = $arguments[0];
-        dd($data);
+//        dd($data);
         return $this->client->$name(array_merge(["auth" => $this->auth], $data));
     }
 }
