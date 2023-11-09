c87f3c3af92801444ce63cb590b12609082e526f
turgutefeyuksel <turgutefeyuksel@gmail.com>
2023-11-09 17:53:53 +0300

    add trendyol create product add attribute

--- app/Services/Merchants/TrendyolMerchant.php
+++ app/Services/Merchants/TrendyolMerchant.php
@@ -104,6 +104,7 @@ class TrendyolMerchant implements Merchant
                 ];
             }
         }
+        dd("efe");
         $response = $this->supplierClient()->send($method, "v2/products", ["json" => [
             "items" => [
                 [
