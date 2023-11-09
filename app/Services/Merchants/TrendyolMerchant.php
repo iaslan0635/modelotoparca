f93a31b83c0b8e34e2e7bb7f500af83af6b69745
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-11-09 14:15:18 +0300

    use Laravel's better Guzzle/Http wrapper for trendyol

--- app/Services/Merchants/TrendyolMerchant.php
+++ app/Services/Merchants/TrendyolMerchant.php
@@ -5,8 +5,9 @@ namespace App\Services\Merchants;
 use App\Enums\OrderRejectReasonType;
 use App\Models\MerchantOrder;
 use App\Models\Product;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
 
 class TrendyolMerchant implements Merchant
 {
@@ -16,15 +17,32 @@ class TrendyolMerchant implements Merchant
 
     public string $supplierId = "";
 
-    protected Client $client;
-
     public function __construct()
     {
         $this->supplierId = config("merchants.trendyol.supplierId");
-        $this->client = new Client([
-            'base_uri' => 'https://api.trendyol.com/sapigw/',
-            "auth" => [config("merchants.trendyol.username"), config("merchants.trendyol.password")]
-        ]);
+    }
+
+    private function client(): PendingRequest
+    {
+        return Http::withBasicAuth(
+            config("merchants.trendyol.username"),
+            config("merchants.trendyol.password")
+        )->baseUrl('https://api.trendyol.com/sapigw/');
+    }
+
+    private function supplierClient(): PendingRequest
+    {
+        return Http::withBasicAuth(
+            config("merchants.trendyol.username"),
+            config("merchants.trendyol.password")
+        )->baseUrl("https://api.trendyol.com/sapigw/suppliers/$this->supplierId/");
+    }
+
+    private function getBatchResponse(Response|string $responseOrBatchId)
+    {
+        $batchId = is_string($responseOrBatchId) ? $responseOrBatchId : $responseOrBatchId->object()->batchRequestId;
+
+        return $this->supplierClient()->get("products/batch-requests/$batchId")->object();
     }
 
     private function formatPrice($price)
@@ -34,30 +52,24 @@ class TrendyolMerchant implements Merchant
 
     public function setStock(Product $product, $stock)
     {
-        $request = $this->client->post("suppliers/$this->supplierId/products/price-and-inventory", ["json" => [
+        return $this->supplierClient()->post("products/price-and-inventory", [
             "items" => [
                 [
                     "barcode" => $product->sku,
                     "quantity" => $stock
                 ]
             ]
-        ]]);
-
-        return json_decode($request->getBody())->batchRequestId;
+        ])->object()->batchRequestId;
     }
 
     public function getOrders()
     {
-        $request = $this->client->get("suppliers/$this->supplierId/orders");
-
-        return json_decode($request->getBody());
+        return $this->supplierClient()->get("orders")->object();
     }
 
     public function getQuestions()
     {
-        $request = $this->client->get("suppliers/$this->supplierId/questions/filter");
-
-        return json_decode($request->getBody());
+        return $this->supplierClient()->get("questions/filter")->object();
     }
 
     public function updateProduct(Product $product)
@@ -67,7 +79,7 @@ class TrendyolMerchant implements Merchant
 
     private function sendProduct(Product $product, string $method)
     {
-        $response = $this->client->request($method, "suppliers/$this->supplierId/v2/products", ["json" => [
+        $response = $this->supplierClient()->send($method, "v2/products", ["json" => [
             "items" => [
                 [
                     "barcode" => $product->sku,
@@ -86,24 +98,24 @@ class TrendyolMerchant implements Merchant
 
                 ]
             ]
-        ]]);
+        ]])->object();
 
         $this->setStock($product, $product->quantity);
         $this->updatePrice($product);
 
-        return json_decode($response->getBody())->batchRequestId;
+        return $response->batchRequestId;
     }
 
     public function updateOrder(MerchantOrder $order)
     {
-        $request = $this->client->get();
+        // TODO: Implement updateOrder() method.
     }
 
     public function updateDeliveryCode($id, $code): bool
     {
-        $this->client->put("suppliers/$this->supplierId/$id/update-tracking-number", ["json" => [
+        $this->supplierClient()->put("$id/update-tracking-number", [
             "trackingNumber" => $code
-        ]]);
+        ]);
 
         return true;
     }
@@ -120,137 +132,131 @@ class TrendyolMerchant implements Merchant
 
     public function getCategories()
     {
-        $request = $this->client->get("product-categories");
-
-        return json_decode($request->getBody());
+        return $this->client()->get("product-categories")->object();
     }
 
     public function getCategoryAttributes($categoryId)
     {
-        $request = $this->client->get("product-categories/$categoryId/attributes");
-
-        return json_decode($request->getBody());
+        return $this->client()->get("product-categories/$categoryId/attributes")->object();
     }
 
     public function getCargoCompanies()
     {
-        return array(
-            array(
+        return [
+            [
                 'Id' => 42,
                 'Code' => 'DHLMP',
                 'Name' => 'DHL Marketplace',
                 'Tax Number' => '951-241-77-13'
-            ),
-            array(
+            ],
+            [
                 'Id' => 38,
                 'Code' => 'SENDEOMP',
                 'Name' => 'Sendeo Marketplace',
                 'Tax Number' => '2910804196'
-            ),
-            array(
+            ],
+            [
                 'Id' => 36,
                 'Code' => 'NETMP',
                 'Name' => 'NetKargo Lojistik Marketplace',
                 'Tax Number' => '6930094440'
-            ),
-            array(
+            ],
+            [
                 'Id' => 34,
                 'Code' => 'MARSMP',
                 'Name' => 'Mars Lojistik Marketplace',
                 'Tax Number' => '6120538808'
-            ),
-            array(
+            ],
+            [
                 'Id' => 39,
                 'Code' => 'BIRGUNDEMP',
                 'Name' => 'Bir Günde Kargo Marketplace',
                 'Tax Number' => '1770545653'
-            ),
-            array(
+            ],
+            [
                 'Id' => 35,
                 'Code' => 'OCTOMP',
                 'Name' => 'Octovan Lojistik Marketplace',
                 'Tax Number' => '6330506845'
-            ),
-            array(
+            ],
+            [
                 'Id' => 30,
                 'Code' => 'BORMP',
                 'Name' => 'Borusan Lojistik Marketplace',
                 'Tax Number' => '1800038254'
-            ),
-            array(
+            ],
+            [
                 'Id' => 12,
                 'Code' => 'UPSMP',
                 'Name' => 'UPS Kargo Marketplace',
                 'Tax Number' => '9170014856'
-            ),
-            array(
+            ],
+            [
                 'Id' => 13,
                 'Code' => 'AGTMP',
                 'Name' => 'AGT Marketplace',
                 'Tax Number' => '6090414309'
-            ),
-            array(
+            ],
+            [
                 'Id' => 14,
                 'Code' => 'CAIMP',
                 'Name' => 'Cainiao Marketplace',
                 'Tax Number' => '0'
-            ),
-            array(
+            ],
+            [
                 'Id' => 10,
                 'Code' => 'MNGMP',
                 'Name' => 'MNG Kargo Marketplace',
                 'Tax Number' => '6080712084'
-            ),
-            array(
+            ],
+            [
                 'Id' => 19,
                 'Code' => 'PTTMP',
                 'Name' => 'PTT Kargo Marketplace',
                 'Tax Number' => '7320068060'
-            ),
-            array(
+            ],
+            [
                 'Id' => 9,
                 'Code' => 'SURATMP',
                 'Name' => 'Sürat Kargo Marketplace',
                 'Tax Number' => '7870233582'
-            ),
-            array(
+            ],
+            [
                 'Id' => 17,
                 'Code' => 'TEXMP',
                 'Name' => 'Trendyol Express Marketplace',
                 'Tax Number' => '8590921777'
-            ),
-            array(
+            ],
+            [
                 'Id' => 6,
                 'Code' => 'HOROZMP',
                 'Name' => 'Horoz Kargo Marketplace',
                 'Tax Number' => '4630097122'
-            ),
-            array(
+            ],
+            [
                 'Id' => 20,
                 'Code' => 'CEVAMP',
                 'Name' => 'CEVA Marketplace',
                 'Tax Number' => '8450298557'
-            ),
-            array(
+            ],
+            [
                 'Id' => 4,
                 'Code' => 'YKMP',
                 'Name' => 'Yurtiçi Kargo Marketplace',
                 'Tax Number' => '3130557669'
-            ),
-            array(
+            ],
+            [
                 'Id' => 7,
                 'Code' => 'ARASMP',
                 'Name' => 'Aras Kargo Marketplace',
                 'Tax Number' => '720039666'
-            )
-        );
+            ]
+        ];
     }
 
     public function getBrands()
     {
-        $request = $this->client->get("brands");
-
-        return json_decode($request->getBody(), true)["brands"];
+        return $this->client()->get("brands")->object();
     }
 
     public function approveOrder(MerchantOrder $order)
@@ -275,29 +281,27 @@ class TrendyolMerchant implements Merchant
 
     public function sendQuestionAnswer(MerchantOrder $question, string $answer): bool
     {
-        $this->client->post("suppliers/$this->supplierId/questions/$question->merchant_id/answers", ["json" => [
+        $this->supplierClient()->post("questions/$question->merchant_id/answers", [
             "text" => $answer
-        ]]);
+        ]);
 
         return true;
     }
 
     public function deleteProduct(Product $product)
     {
-        $request = $this->client->delete("suppliers/$this->supplierId/v2/products", ["json" => [
+        return $this->supplierClient()->delete("v2/products", [
             "items" => [
                 [
                     "barcode" => $product->sku
                 ]
             ]
-        ]]);
-
-        return json_decode($request->getBody())->batchRequestId;
+        ])->object()->batchRequestId;
     }
 
     public function updatePrice(Product $product)
     {
-        $request = $this->client->post("suppliers/$this->supplierId/products/price-and-inventory", ["json" => [
+        return $this->supplierClient()->post("products/price-and-inventory", [
             "items" => [
                 [
                     "barcode" => $product->sku,
@@ -305,11 +309,7 @@ class TrendyolMerchant implements Merchant
                     "listPrice" => $this->formatPrice($product->price->discounted_price),
                 ]
             ]
-        ]]);
-
-        $body = json_decode($request->getBody());
-
-        return $body->batchRequestId;
+        ])->object()->batchRequestId;
     }
 
     public function syncOrders()
