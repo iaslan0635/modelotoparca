898836c3f11be3bbbf3a4c5623694ccb0098f66d
turgutefeyuksel <turgutefeyuksel@gmail.com>
2023-11-09 16:42:45 +0300

    bug fixed hepsiburada

--- app/Services/Merchants/Hepsiburada.php
+++ app/Services/Merchants/Hepsiburada.php
@@ -165,6 +165,8 @@ class Hepsiburada implements Merchant
 
     public function getCategoryAttributeValues($categoryId, $attributeId)
     {
+        $page = 0;
+        $size = 1000;
         $request = $this->client->get("https://mpop.hepsiburada.com/product/api/categories/$categoryId/attribute/$attributeId/values?version=4&page=0&size=1000");
 
         return json_decode($request->getBody());
