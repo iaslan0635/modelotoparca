51eef3249958dc1fbbe369281d0622b46341ffaa
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-11-09 18:59:18 +0300

    n11

--- resources/views/livewire/admin/product/create-merchant.blade.php
+++ resources/views/livewire/admin/product/create-merchant.blade.php
@@ -2,16 +2,19 @@
     @if(!$product->merchants()->where('merchant', 'n11')->exists())
         <button wire:dirty.attr="disabled" wire:click="createN11" class="btn btn-sm fw-bold btn-primary">
             N11 oluştur
+            <span wire:loading><i class="fas fa-spinner fa-spin"></i></span>
         </button>
     @endif
     @if(!$product->merchants()->where('merchant', 'hepsiburada')->exists())
         <button wire:dirty.attr="disabled" wire:click="createHepsiburada" class="btn btn-sm fw-bold btn-primary">
             Hepsiburada oluştur
+            <span wire:loading><i class="fas fa-spinner fa-spin"></i></span>
         </button>
     @endif
     @if(!$product->merchants()->where('merchant', 'trendyol')->exists())
         <button wire:dirty.attr="disabled" wire:click="createTrendyol" class="btn btn-sm fw-bold btn-primary">
             Trendyol oluştur
+            <span wire:loading><i class="fas fa-spinner fa-spin"></i></span>
         </button>
     @endif
 </div>
