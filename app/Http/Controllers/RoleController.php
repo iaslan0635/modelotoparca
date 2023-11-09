1a70efbbc7845ac378a58a0f0c613044149cd735
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-11-09 10:07:10 +0300

    permissions & roles

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Spatie\Permission\Models\Role;
+
 class RoleController extends Controller
 {
     public function index()
     {
-        return view("admin.apps.user-management.roles.list");
+        $roles = Role::with("permissions")->get();
+        return view("admin.apps.user-management.roles.list", compact('roles'));
     }
 
     public function show()
     {
         return view("admin.apps.user-management.roles.view");
+    }
+
+    public function edit()
+    {
+        return view("admin.apps.user-management.roles.edit");
     }
 }
