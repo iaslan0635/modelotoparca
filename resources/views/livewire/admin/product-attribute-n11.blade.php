efbbc3fade5f12c341d6e879a67eb8c55f2565b9
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-11-10 17:30:40 +0300

    fix livewire

--- resources/views/livewire/admin/product-attribute-n11.blade.php
+++ resources/views/livewire/admin/product-attribute-n11.blade.php
@@ -1,22 +1,19 @@
-@if(($name = $attribute?->name ?? $attribute['name']) !== "Marka")
-    <tr>
-        <td>{{ $name }}</td>
-        <td>{!! $attribute['mandatory'] ? "<b>Evet</b>":"Hayır" !!}</td>
-        <td>
-            @if($attribute['customValue'])
-                <input type="text" wire:model="value">
-            @else
-                <select wire:model="value"
-                        class="form-control">
-                    @foreach($attribute['valueList'] as $value)
-                        <option value="{{ $value->name }}">{{ $value->name }}</option>
-                    @endforeach
-                </select>
-            @endif
-        </td>
-        <td>
-            <button wire:click="save" class="btn btn-success btn-sm">Kaydet</button>
-        </td>
-    </tr>
-@endif
-
+<tr>
+    <td>{{ $name }}</td>
+    <td>{!! $attribute['mandatory'] ? "<b>Evet</b>":"Hayır" !!}</td>
+    <td>
+        @if($attribute['customValue'])
+            <input type="text" wire:model="value">
+        @else
+            <select wire:model="value"
+                    class="form-control">
+                @foreach($attribute['valueList'] as $value)
+                    <option value="{{ $value->name }}">{{ $value->name }}</option>
+                @endforeach
+            </select>
+        @endif
+    </td>
+    <td>
+        <button wire:click="save" class="btn btn-success btn-sm">Kaydet</button>
+    </td>
+</tr>
