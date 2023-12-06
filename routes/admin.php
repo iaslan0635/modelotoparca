000fae8a9b60441be5b3c2b8071a9b126cd1f7a4
lifecodeof <m.semihkoyuncu01@gmail.com>
2023-12-06 17:04:25 +0300

    .

--- routes/admin.php
+++ routes/admin.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'admin.index');
 
-
 Route::prefix('products/{product}/edit')->name('products.edit.')->controller(ProductController::class)->group(function () {
     Route::get('oem', 'push_oem')->name('oem');
     Route::post('image', 'push_image')->name('image');
