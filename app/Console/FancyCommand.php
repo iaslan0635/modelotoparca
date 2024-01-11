5d617ff0411e268879f2b2f3031f3f59d251175b
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-01-11 13:28:26 +0300

    .

---
+++ app/Console/FancyCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleSectionOutput;
+
+class FancyCommand extends Command
+{
+    function iterate($iterable, bool $transistent = true)
+    {
+        /** @var ConsoleSectionOutput $section */
+        $section = $this->output->getOutput()->section();
+        $bar = new ProgressBar($section, count($iterable));
+        $bar->start();
+
+        foreach ($iterable as $value) {
+            yield $value;
+            $bar->advance();
+        }
+
+        if ($transistent)
+            $section->clear();
+    }
+}
