81da46247ad32326858a11d08b8167263eae7f8d
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-01-11 12:12:04 +0300

    .

---
+++ database/migrations/2024_01_11_121134_add_from_bot_to_images.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->boolean('from_bot')->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropColumn('from_bot');
+        });
+    }
+};
