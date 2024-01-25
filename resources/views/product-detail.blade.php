c04b310a8dd4a6ebf9e6750cb4c6767e084fe5df
ishak <iaslan3506@gmail.com>
2024-01-25 16:24:13 +0300

    test

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -400,9 +400,9 @@
                                                                                                     $car->produced_to ?? $cachedNow ?? ($cachedNow = now()->format("Y-m-d"))
                                                                                                 }}
                                                                                     </td>
-                                                                                    <td class="analogs-table__column">{{ $car->engine }} </td>
+{{--                                                                                    <td class="analogs-table__column">{{ $car->engine }} </td>--}}
+                                                                                    <td class="analogs-table__column">{{$car->type}} / {{ $car->power_kw }} Kw / {{ $car->power_hp }} Hp </td>
                                                                                     <td class="analogs-table__column">{{ $car->capacity }} cc </td>
-{{--                                                                                    <td class="analogs-table__column">{{ $car->power_kw }} Kw / {{ $car->power_hp }} Hp </td>--}}
                                                                                     <td class="analogs-table__column"> {{$car->engine_code}} </td>
                                                                                 </tr>
                                                                             @endforeach
