d494240753bcbddf2c70d00db4ab7692435a1b46
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-01-31 15:47:23 +0300

    .

--- resources/views/admin/inhouse/user/index.blade.php
+++ resources/views/admin/inhouse/user/index.blade.php
@@ -42,15 +42,6 @@
                     <!--end::Add user-->
                 </div>
                 <!--end::Toolbar-->
-                <!--begin::Group actions-->
-                <div class="d-flex justify-content-end align-items-center d-none" data-kt-user-table-toolbar="selected">
-                    <div class="fw-bold me-5">
-                        <span class="me-2" data-kt-user-table-select="selected_count"></span>
-                        Selected
-                    </div>
-                    <button type="button" class="btn btn-danger" data-kt-user-table-select="delete_selected">Delete Selected</button>
-                </div>
-                <!--end::Group actions-->
                 <!--begin::Modal - Add task-->
                 <div class="modal fade" id="kt_modal_add_user" tabindex="-1" aria-hidden="true">
                     <!--begin::Modal dialog-->
@@ -142,11 +133,13 @@
                 <thead>
                 <!--begin::Table row-->
                 <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
+                    {{--
                     <th class="w-10px pe-2">
                         <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
                             <input class="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_table_users .form-check-input" value="1"/>
                         </div>
                     </th>
+                    --}}
                     <th class="min-w-125px">User</th>
                     <th class="min-w-125px">Joined Date</th>
                     <th class="text-end min-w-100px">Actions</th>
@@ -159,6 +152,7 @@
                 @foreach($users as $user)
                     <!--begin::Table row-->
                     <tr>
+                        {{--
                         <!--begin::Checkbox-->
                         <td>
                             <div class="form-check form-check-sm form-check-custom form-check-solid">
@@ -166,6 +160,7 @@
                             </div>
                         </td>
                         <!--end::Checkbox-->
+                        --}}
                         <!--begin::User=-->
                         <td class="d-flex align-items-center">
                             <!--begin:: Avatar -->
