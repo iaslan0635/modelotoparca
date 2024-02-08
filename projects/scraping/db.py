fc49a463d7a0f75bdd3c27c50cf059266c7dc6aa
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-02-08 13:36:36 +0300

    Add mysqlclient package and update database connection in db.py

--- projects/scraping/db.py
+++ projects/scraping/db.py
@@ -1,19 +1,28 @@
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, create_engine
 from dotenv import dotenv_values
-import os
 
-env = dotenv_values(".env")
-db_uri = f"{env['DB_CONNECTION']}://{env['DB_USERNAME']}:{env['DB_PASSWORD']}@{env['DB_HOST']}/{env['DB_DATABASE']}"
+env = dotenv_values("../../.env")
+uri = f"mysql+mysqldb://{env['DB_USERNAME']}:{env['DB_PASSWORD']}@{env['DB_HOST']}:{env['DB_PORT']}/{env['DB_DATABASE']}"
 
 
-engine = create_engine(db_uri)
-Base = automap_base()
-Base.prepare(engine, reflect=True)
+engine = create_engine(uri)
 
-# mapped classes are now created with names by default
-# matching that of the table name.
-LoginData = Base.classes.login_data
+Session = sessionmaker(engine)
 
-db_session = Session(engine)
+
+class Base(DeclarativeBase): ...
+
+
+class Image(Base):
+    __tablename__ = "images"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    imageable_type: Mapped[str] = mapped_column()
+    imageable_id: Mapped[int]
+    path: Mapped[str]
+
+
+class Car(Base):
+    __tablename__ = "cars"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    permalink: Mapped[str]
