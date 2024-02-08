1fb5288cffbbfaa9d9815558fd17ef481f9d6350
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-02-08 11:20:08 +0300

    remove type attribute from users

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -1199,7 +1199,6 @@
 												<!--begin::Username-->
 												<div class="d-flex flex-column">
 													<div class="fw-bold d-flex align-items-center fs-5">{{Auth::user()->first_name}} {{Auth::user()->last_name }}
-													<span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">{{Auth::user()->type}}</span></div>
 													<a href="#" class="fw-semibold text-muted text-hover-primary fs-7">{{Auth::user()->email}}</a>
 												</div>
 												<!--end::Username-->
