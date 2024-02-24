3515807cba438ffb82036c7d729e073679c37d7c
ishak <iaslan3506@gmail.com>
2024-02-24 11:59:34 +0300

    role

--- config/permissions.php
+++ config/permissions.php
@@ -4,20 +4,9 @@ return [
     [
         'name' => 'Ürünler',
         'children' => [
-            ['name' => 'Arama'],
-            ['name' => 'Filtrele'],
             ['name' => 'Listele'],
-            ['name' => 'Düzenle',
-                'children' => [
-                    ['name' => 'BotResimleri'],
-                    ['name' => 'Stok'],
-                    ['name' => 'UyumluAraçEkle'],
-                    ['name' => 'BotİleYenidenÇek'],
-                    ['name' => 'HepsiburadaOluştur'],
-                    ['name' => 'TrendyolOluştur'],
-                    ['name' => 'N11Oluştur'],
-                ]
-            ],
+            ['name' => 'Ekle'],
+            ['name' => 'Düzenle'],
             ['name' => 'Sil'],
             ['name' => 'Stok',
                 'children' => [
@@ -27,18 +16,13 @@ return [
             ],
         ]
     ],
-    ['name' => 'Kategoriler',
+    [
+    'name' => 'Kategoriler',
     'children' => [
         ['name' => 'Listele'],
         ['name' => 'Ekle'],
         ['name' => 'Düzenle'],
         ['name' => 'Sil'],
-    ]],
-    ['name' => 'Markalar',
-    'children' => [
-        ['name' => 'Listele'],
-        ['name' => 'Ekle'],
-        ['name' => 'Düzenle'],
-        ['name' => 'Sil'],
-    ]]
+    ]
+]
 ];
