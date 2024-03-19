7b9b38f8a11e286a7664af62619b3fa184fdda44
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-03-19 18:46:05 +0300

    sidebar

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -2,9 +2,15 @@
 
 <div class="menu-item">
     <a class="menu-link" href="{{ $href }}">
-        <span class="menu-bullet">
-            <span class="bullet bullet-dot"></span>
-        </span>
+        @isset($icon)
+            <span class="menu-icon">
+                {{ $icon }}
+            </span>
+        @else
+            <span class="menu-bullet">
+                <span class="bullet bullet-dot"></span>
+            </span>
+        @endisset
         <span class="menu-title">{{ $slot }}</span>
     </a>
 </div>
