971675e97c26eb65bfa5589ce223f046b595abab
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-03-23 12:52:02 +0300

    add info

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -135,11 +135,11 @@
                                                     <td>
                                                         <h6>
                                                             <span
-                                                                class="badge @if($tiger->stock_on_51) badge-success @else badge-danger @endif">S51</span>
+                                                                class="badge @if($tiger?->stock_on_51) badge-success @else badge-danger @endif">S51</span>
                                                             <span
-                                                                class="badge @if($tiger->stock_on_38) badge-success @else badge-danger @endif">S38</span>
+                                                                class="badge @if($tiger?->stock_on_38) badge-success @else badge-danger @endif">S38</span>
                                                             <span
-                                                                class="badge @if($tiger->stock_on_01) badge-success @else badge-danger @endif">S01</span>
+                                                                class="badge @if($tiger?->stock_on_01) badge-success @else badge-danger @endif">S01</span>
                                                         </h6>
                                                     </td>
                                                 </tr>
