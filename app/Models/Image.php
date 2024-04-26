25a295e549ce3c322361bd2611ae9e9b126b7fc5
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-04-26 11:41:46 +0300

    add absolute path to image

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,11 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Image extends BaseModel
 {
     protected function url(): Attribute
     {
-        return Attribute::get(fn () => asset("storage/$this->path"));
+        return Attribute::get(fn() => asset("storage/$this->path"));
+    }
+
+    protected function absolutePath(): Attribute
+    {
+        return Attribute::get(fn() => storage_path(app()->joinPaths("app", $this->path)));
     }
 }
