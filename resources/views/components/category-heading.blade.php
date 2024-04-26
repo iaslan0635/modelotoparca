6ff31ac36641b183fe79ec71a45a958e8d9a6064
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-04-26 14:09:17 +0300

    Revert "image slider"
    This reverts commit bd389516e311af5e72f258abdbd117c00b2c4abf.

--- resources/views/components/category-heading.blade.php
+++ resources/views/components/category-heading.blade.php
@@ -35,7 +35,17 @@
             loop: true,
             margin: 10,
             dots: true,
-            items: 3
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 5
+                }
+            }
         })
 
         function selectBrand(id) {
