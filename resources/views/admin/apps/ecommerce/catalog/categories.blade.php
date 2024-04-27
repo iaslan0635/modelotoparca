26d0c029697896a4e015ea8887101586fd44c5fb
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-04-27 13:25:40 +0300

    category pagination

--- resources/views/admin/apps/ecommerce/catalog/categories.blade.php
+++ resources/views/admin/apps/ecommerce/catalog/categories.blade.php
@@ -195,7 +195,6 @@
                                     </div>
                                 </th>
                                 <th class="min-w-250px">Category</th>
-                                <th class="text-end min-w-70px">Actions</th>
                             </tr>
                             <!--end::Table row-->
                             </thead>
