49aa42bcebd2342df24bb87557ecb57c1d15bb7a
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-05-09 14:54:49 +0300

    migrate

---
+++ database/migrations/2024_05_09_115338_fix_unique_in_search_pages.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    protected $connection = 'bigdata';
+
+    public function up(): void
+    {
+        Schema::table('search_pages', function (Blueprint $table) {
+            $table->dropUnique('search_pages_keyword_unique');
+            $table->unique('url');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('search_pages', function (Blueprint $table) {
+            $table->dropUnique('search_pages_url_unique');
+            $table->unique('keyword');
+        });
+    }
+};
