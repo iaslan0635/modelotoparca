c9b92a7b9655f8cb275e17c236357ae604a131f1
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-05-10 13:27:50 +0300

    migrate

---
+++ database/migrations/2024_05_10_102624_add_engine_code_to_cars.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    protected $connection = 'bigdata';
+
+    public function up(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->string('engine_code')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('cars', function (Blueprint $table) {
+            $table->dropColumn('engine_code');
+        });
+    }
+};
