0f1812803bafd8ab4895a629a6038a71eada83f2
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-05-29 14:12:57 +0300

    fix search

--- app/Packages/Search.php
+++ app/Packages/Search.php
@@ -243,7 +243,7 @@ class Search
             if (empty($word)) {
                 continue;
             }
-            $query->must(Query::prefix()->field('full_text')->value($word)->caseInsensitive(true));
+            $query->must(Query::term()->field('full_text')->value($word)->caseInsensitive(true));
         }
 
         return $query;
