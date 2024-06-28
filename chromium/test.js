d80dc03d8eae1bd028416c1b21edd114179f8328
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-06-28 10:55:22 +0300

    add chromium and update ocp client

--- chromium/test.js
+++ chromium/test.js
@@ -1,3 +1,15 @@
 import {request} from "./chromium.js";
 
-request("https://www.onlinecarparts.co.uk/ajax/search/autocomplete?keyword=GUA+40700").then(console.log)
+const {
+    headers,
+    status,
+    body,
+} = request("https://www.onlinecarparts.co.uk/ajax/search/autocomplete?keyword=GUA+40700")
+
+const text = body.toString()
+
+console.log(`
+Headers: ${JSON.stringify(headers)}
+Status: ${status}
+Body: ${text}
+`)
