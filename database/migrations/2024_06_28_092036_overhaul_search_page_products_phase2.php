918229efa1d5c6eb28706b31cdbeef2e5c6777c9
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-06-28 12:22:42 +0300

    refactor database

--- database/migrations/2024_06_28_092036_overhaul_search_page_products_phase2.php
+++ database/migrations/2024_06_28_092036_overhaul_search_page_products_phase2.php
@@ -10,16 +10,22 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('search_page_products', function (Blueprint $table) {
-            $table->string("url")->index()->nullable();
-            $table->string("article_no")->index()->nullable();
+            $table->string("url")->index()->change();
+            $table->string("article_no")->index()->change();
+
+            $table->dropColumn("product_id");
+            $table->dropColumn("brand_id");
         });
     }
 
     public function down(): void
     {
         Schema::table('search_page_products', function (Blueprint $table) {
-            $table->dropColumn("url");
-            $table->dropColumn("article_no");
+            $table->unsignedBigInteger("product_id");
+            $table->unsignedBigInteger("brand_id");
+
+            $table->string("url")->index()->nullable()->change();
+            $table->string("article_no")->index()->nullable()->change();
         });
     }
 };
