6f91e83bd0db5979fd5ef59077f24c31af39ad88
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-07-01 16:01:39 +0300

    minor fix

--- app/Facades/DiscountFacade.php
+++ app/Facades/DiscountFacade.php
@@ -2,17 +2,10 @@
 
 namespace App\Facades;
 
-use InvalidArgumentException;
-
 class DiscountFacade
 {
-    public static function reverseCalculate(float $discountedPrice, float $discountPercentage) {
-        // Ensure the discount percentage is between 0 and 100
-        if ($discountPercentage < 0 || $discountPercentage > 100) {
-            throw new InvalidArgumentException("Discount percentage must be between 0 and 100.");
-        }
-
-        // Calculate the original price
-        return $discountedPrice / (1 - ($discountPercentage / 100));
+    public static function reverseCalculate(float|int|string $discountedPrice, float|int|string $discountPercentage)
+    {
+        return bcdiv($discountedPrice, bcsub(1, $discountPercentage, 2), 2);
     }
 }
