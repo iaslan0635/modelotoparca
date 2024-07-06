a0545084dce574258e2297bd2c937bebe3cd32ae
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-07-06 11:28:13 +0300

    search translations

--- lang/tr/highlights.php
+++ lang/tr/highlights.php
@@ -1,21 +1,23 @@
 <?php
 
 return [
+    'brand.name' => 'Markadan bulundu',
+
+    'producercode' => 'Üretici kodundan bulundu',
+    'producercode_unbranded' => 'Markasız üretici kodundan bulundu',
+    'part_number' => 'Parça numarasından bulundu',
+    'cross_code' => 'Karşılaştırma kodundan bulundu',
+    'producercode2' => "Üretici kodu 2'den bulundu",
+    'hidden_searchable' => 'Gizli aranabilir verilerden bulundu',
+    'tecdoc' => "Tecdoc'dan bulundu",
+
+    'categories.name' => 'Kategoriden bulundu',
+
     'title' => 'Başlıkdan bulundu',
     'sub_title' => 'Alt başlıktan bulundu',
-    'full_text' => 'Birleşik verilerden bulundu',
-    'cross_code' => 'Karşılaştırma kodundan bulundu',
-    'producercode' => 'Üretici kodundan bulundu',
-    'producercode2' => "Üretici kodu 2'den bulundu",
-    'cross_code_regex' => 'Karşılaştırma kodundan bulundu',
-    'producercode_regex' => 'Üretici kodundan bulundu',
-    'producercode2_regex' => "Üretici kodu 2'den bulundu",
-    'similar_product_codes' => 'Benzer ürün kodlarından bulundu',
-    'oems.oem' => 'Oem kodundan bulundu',
-    'cars.name' => 'Araç modelinden bulundu',
-    'cars.regex_name' => 'Araç modelinden bulundu',
-    'tecdoc.name' => "Tecdoc'dan bulundu",
-    'tecdoc.name_regex' => "Tecdoc'dan bulundu",
-    'hidden_searchable' => 'Gizli aranabilir verilerden bulundu',
+    'description' => 'Açıklamadan bulundu',
 
+    'cars.name' => 'Araç modelinden bulundu',
+    'oems' => 'Oem kodundan bulundu',
+    'similars' => 'Benzer ürün kodlarından bulundu',
 ];
