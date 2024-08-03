9005d1f5ac45757399038206bd84d7b4b5993267
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-08-03 12:12:59 +0300

    log errors

--- app/Facades/DiscountFacade.php
+++ app/Facades/DiscountFacade.php
@@ -9,10 +9,11 @@ class DiscountFacade
 {
     public static function reverseCalculate(float|int|string $discountedPrice, float|int|string $discountPercentage)
     {
-        if (!is_numeric($discountedPrice) || !is_numeric($discountPercentage)) {
-            Log::channel("important")->error("Both arguments must be numeric discountedPrice: $discountedPrice discountPercentage: $discountPercentage");
+        try {
+            return bcdiv($discountedPrice, bcsub(1, $discountPercentage, 2), 2);
+        } catch (\Throwable $e) {
+            Log::channel("important")->error("Error while reverse calculating discount: $e, discountedPrice: $discountedPrice discountPercentage: $discountPercentage");
+            report($e);
         }
-
-        return bcdiv($discountedPrice, bcsub(1, $discountPercentage, 2), 2);
     }
 }
