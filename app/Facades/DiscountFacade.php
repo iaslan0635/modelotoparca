0ddfa3b4fae5548bb9068c8c5f9f887a6ea19468
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-08-13 14:44:48 +0300

    fix bcmath scientific notation

--- app/Facades/DiscountFacade.php
+++ app/Facades/DiscountFacade.php
@@ -9,6 +9,9 @@ class DiscountFacade
 {
     public static function reverseCalculate(float|int|string $discountedPrice, float|int|string $discountPercentage)
     {
+        if (!is_string($discountedPrice))
+            $discountedPrice = number_format($discountedPrice, 2, thousands_separator: '');
+
         try {
             return bcdiv($discountedPrice, bcsub(1, $discountPercentage, 2), 2);
         } catch (\Throwable $e) {
