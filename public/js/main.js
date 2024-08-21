a733cfe51714614af7112af81b8c37bbb1d8f0b0
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-08-21 12:47:50 +0300

    fix owl carousel js

--- public/js/main.js
+++ public/js/main.js
@@ -854,80 +854,22 @@
             const layout = $(this).data('layout');
             const owlCarousel = $(this).find('.owl-carousel');
 
+            let options;
+            try {
+                options = JSON.parse(owlCarousel.attr("data-options"));
+            } catch {
+                options = {};
+            }
+
             owlCarousel.owlCarousel(Object.assign({}, {
-                dots: false,
-                margin: 20,
-                loop: true,
-                rtl: isRTL()
-            }, carouselOptions[layout]));
+                dots: false, margin: 20, loop: true, rtl: isRTL(),
+            }, carouselOptions[layout]), options);
 
             $(this).find('.section-header__arrow--prev').on('click', function () {
                 owlCarousel.trigger('prev.owl.carousel', [500]);
             });
             $(this).find('.section-header__arrow--next').on('click', function () {
                 owlCarousel.trigger('next.owl.carousel', [500]);
-            });
-
-            let cancelPreviousGroupChange = function () {
-            };
-
-            $(this).find('.section-header__groups-button').on('click', function () {
-                const carousel = block.find('.block-products-carousel__carousel');
-
-                if ($(this).is('.section-header__groups-button--active')) {
-                    return;
-                }
-
-                cancelPreviousGroupChange();
-
-                $(this).closest('.section-header__groups').find('.section-header__groups-button').removeClass('section-header__groups-button--active');
-                $(this).addClass('section-header__groups-button--active');
-
-                carousel.addClass('block-products-carousel__carousel--loading');
-
-                // timeout ONLY_FOR_DEMO! you can replace it with an ajax request
-                let timer;
-                timer = setTimeout(function () {
-                    let items = block.find('.owl-carousel .owl-item:not(".cloned") .block-products-carousel__column');
-
-                    /*** this is ONLY_FOR_DEMO! / start */
-                    /**/
-                    const itemsArray = items.get();
-                    /**/
-                    const newItemsArray = [];
-                    /**/
-                    /**/
-                    while (itemsArray.length > 0) {
-                        /**/
-                        const randomIndex = Math.floor(Math.random() * itemsArray.length);
-                        /**/
-                        const randomItem = itemsArray.splice(randomIndex, 1)[0];
-                        /**/
-                        /**/
-                        newItemsArray.push(randomItem);
-                        /**/
-                    }
-                    /**/
-                    items = $(newItemsArray);
-                    /*** this is ONLY_FOR_DEMO! / end */
-
-                    block.find('.owl-carousel')
-                        .trigger('replace.owl.carousel', [items])
-                        .trigger('refresh.owl.carousel')
-                        .trigger('to.owl.carousel', [0, 0]);
-
-                    $('.product-card__action--quickview', block).on('click', function () {
-                        quickview.clickHandler.apply(this, arguments);
-                    });
-
-                    carousel.removeClass('block-products-carousel__carousel--loading');
-                }, 1000);
-                cancelPreviousGroupChange = function () {
-                    // timeout ONLY_FOR_DEMO!
-                    clearTimeout(timer);
-                    cancelPreviousGroupChange = function () {
-                    };
-                };
             });
         });
     });
