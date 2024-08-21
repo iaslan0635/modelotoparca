1273e28bf84d3a71c25189b0056f510939f2b225
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-08-21 11:20:47 +0300

    fixed height for brand carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -209,7 +209,7 @@
                 </div>
                 <div class="owl-carousel">
                     @foreach(\App\Models\Brand::has('products')->inRandomOrder()->limit(12)->get() as $brand)
-                        <div class="block-products-carousel__column">
+                        <div class="block-products-carousel__column" style="height: 120px">
                             <div class="block-products-carousel__cell">
                                 <div class="product-card product-card--layout--grid">
                                     <div>
