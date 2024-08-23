7b409424b6ea6042859ba178fc4ad98d61228325
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-08-23 12:24:54 +0300

    refactor styles to class

--- resources/views/brands.blade.php
+++ resources/views/brands.blade.php
@@ -4,23 +4,42 @@
     <div class="site__body">
         <x-breadcrumb :parts="[
             ['name' => 'Markalar']
-        ]" />
-    <div class="block block-brands">
-        <div class="container">
-            <ul class="block-brands__list" style="justify-content: center; border: none">
-                @foreach($brands as $brand)
-                    <li class="block-brands__item">
-                        <a href="{{ route('brand.show', $brand) }}" class="block-brands__item-link" style="width: 150px; height: 120px; display: flex; flex-direction: column; justify-content: space-between">
-                            <div style="display: flex; flex-direction: column; justify-content: center; flex: 1">
-                                <img src="{{ $brand->imageUrl() }}">
-                            </div>
-                            <span class="block-brands__item-name">{{$brand->name}}</span>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
+        ]"/>
+        <div class="block block-brands">
+            <div class="container">
+                <ul class="block-brands__list" style="justify-content: center; border: none">
+                    @foreach($brands as $brand)
+                        <li class="block-brands__item">
+                            <a href="{{ route('brand.show', $brand) }}" class="block-brands__item-link brand-item">
+                                <div class="brand-item-inner">
+                                    <img src="{{ $brand->imageUrl() }}">
+                                </div>
+                                <span class="block-brands__item-name">{{$brand->name}}</span>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
-    <div class="block-space block-space--layout--divider-nl"></div>
+        <div class="block-space block-space--layout--divider-nl"></div>
     </div>
 @endsection
+
+@push("styles")
+    <style>
+        .brand-item {
+            width: 150px;
+            height: 120px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+
+        .brand-item-inner {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+        }
+    </style>
+@endpush
