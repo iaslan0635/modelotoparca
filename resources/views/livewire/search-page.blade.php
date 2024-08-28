46afe65799a279480cb53d6699fb0644b09ab483
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-08-28 10:56:54 +0300

    remove sidebar filters

--- resources/views/livewire/search-page.blade.php
+++ resources/views/livewire/search-page.blade.php
@@ -29,42 +29,6 @@
                                             <div class="filter filter--opened" data-collapse-item>
                                                 <button type="button" class="filter__title"
                                                         data-collapse-trigger>
-                                                    Kategoriler
-                                                    <span class="filter__arrow">
-                                                        <svg width="12px" height="7px">
-                                                            <path
-                                                                d="M0.286,0.273 L0.286,0.273 C-0.070,0.629 -0.075,1.204 0.276,1.565 L5.516,6.993 L10.757,1.565 C11.108,1.204 11.103,0.629 10.747,0.273 L10.747,0.273 C10.385,-0.089 9.796,-0.086 9.437,0.279 L5.516,4.296 L1.596,0.279 C1.237,-0.086 0.648,-0.089 0.286,0.273 Z"/>
-                                                        </svg>
-                                                    </span>
-                                                </button>
-                                                <div class="filter__body" data-collapse-content>
-                                                    <div class="filter__container">
-                                                        <div class="filter-categories">
-                                                            <ul class="filter-categories__list">
-                                                                @if($category)
-                                                                    <li class="filter-categories__item filter-categories__item--current">
-                                                                        <a href="#"
-                                                                           wire:click="cleanCategory">Geri Dön
-                                                                        </a>
-                                                                    </li>
-                                                                @endif
-                                                                @foreach($categories->sortBy("name") as ["category" => $category, "count" => $count])
-                                                                    <li class="filter-categories__item filter-categories__item--current">
-                                                                        <a href="#" wire:click="changeCategory({{ $category->id }})">{{ $category->name }}</a>
-                                                                        <div
-                                                                            class="filter-categories__counter">{{ $count }}</div>
-                                                                    </li>
-                                                                @endforeach
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="widget-filters__item">
-                                            <div class="filter filter--opened" data-collapse-item>
-                                                <button type="button" class="filter__title"
-                                                        data-collapse-trigger>
                                                     Fiyat
                                                     <span class="filter__arrow">
                                                         <svg width="12px" height="7px">
@@ -90,56 +54,6 @@
                                                                        name="max_price"
                                                                        value="{{ number_format(request()->query('max_price', 99999), thousands_separator: "") }}"
                                                                        id="max-value" placeholder="Max. Fiyat">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="widget-filters__item">
-                                            <div class="filter filter--opened" data-collapse-item>
-                                                <button type="button" class="filter__title"
-                                                        data-collapse-trigger>
-                                                    Markalar
-                                                    <span class="filter__arrow">
-                                                        <svg width="12px" height="7px">
-                                                            <path
-                                                                d="M0.286,0.273 L0.286,0.273 C-0.070,0.629 -0.075,1.204 0.276,1.565 L5.516,6.993 L10.757,1.565 C11.108,1.204 11.103,0.629 10.747,0.273 L10.747,0.273 C10.385,-0.089 9.796,-0.086 9.437,0.279 L5.516,4.296 L1.596,0.279 C1.237,-0.086 0.648,-0.089 0.286,0.273 Z"/>
-                                                        </svg>
-                                                    </span>
-                                                </button>
-                                                <div class="filter__body" data-collapse-content style="max-height: 175px;overflow: auto;">
-                                                    <div class="filter__container">
-                                                        <div class="filter-list">
-                                                            <div class="filter-list__list">
-                                                                @foreach($brands as $key => $brand)
-                                                                    <label class="filter-list__item ">
-                                                                        <span
-                                                                            class="input-check filter-list__input">
-                                                                            <span class="input-check__body">
-                                                                                <input class="input-check__input"
-                                                                                       name="brands[]"
-                                                                                       value="{{ $key }}"
-                                                                                       {{ $brandFilters ? in_array($key, $brandFilters) ? "checked":null:null }}
-                                                                                       type="checkbox">
-                                                                                <span
-                                                                                    class="input-check__box"></span>
-                                                                                <span class="input-check__icon">
-                                                                                    <svg
-                                                                                        width="9px" height="7px">
-                                                                                        <path
-                                                                                            d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z"/>
-                                                                                    </svg>
-                                                                                </span>
-                                                                            </span>
-                                                                        </span>
-                                                                        <span class="filter-list__title">
-                                                                            {{ $brand["brand"]->name }}
-                                                                        </span>
-                                                                        <span
-                                                                            class="filter-list__counter">{{ $brand["count"] }}</span>
-                                                                    </label>
-                                                                @endforeach
                                                             </div>
                                                         </div>
                                                     </div>
