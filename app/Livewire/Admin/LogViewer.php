855784437d1b4d1191b4bf6fc4fec8b12947fd5f
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-08-30 16:37:56 +0300

    log vieqwr

--- app/Livewire/Admin/LogViewer.php
+++ app/Livewire/Admin/LogViewer.php
@@ -46,6 +46,6 @@ class LogViewer extends Component
     private function generateRandomPastelColor() {
         $hue = mt_rand(0, 35);
 
-        return "hsl({$hue}0deg, 100%, 70%)";
+        return "hsl({$hue}0deg, 100%, 80%)";
     }
 }
