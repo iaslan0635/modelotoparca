c19d5b377d2203534afbc822702c62a29978f7fe
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-08-30 18:55:52 +0300

    sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -8,7 +8,7 @@
                 <div class="widget-filters__item">
                     <div class="widget-filters__item-title">
                         <a style="color: white; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; display: block" href="{{ route('category.show', $category->slug) }}">
-                            <img src="{{ $category->imageUrl() }}" style="height: 2em; margin-right: .5rem">
+                            <img src="{{ $category->imageUrl() }}" style="height: 3em; margin-right: .5rem">
                             {{ $category->name }}
                         </a>
                     </div>
