f7fddd4640211026f864706a13c63deab195dab7
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-09-04 15:09:59 +0300

    add collapses to sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -5,14 +5,42 @@
         </div>
         <div class="widget-filters__list">
             @foreach($categories as $category)
+                @php
+                    $id = "c-collapse-$category->id";
+                    $hasChildren = $category->children->isNotEmpty();
+                @endphp
                 <div class="widget-filters__item py-0 border-0">
                     <div class="widget-filters__item-title">
-                        <a style="color: white; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; display: block" href="{{ route('category.show', $category->slug) }}">
+                        <a
+                            style="color: white; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; display: block"
+                            @if($hasChildren)
+                                data-toggle="collapse" href="#{{$id}}"
+                            @else
+                                href="{{ route('category.show', $category->slug) }}"
+                            @endif
+                        >
                             <img src="{{ $category->imageUrl() }}" style="max-height: 2.8em; margin-right: .5rem; max-width: 2.8em;">
-                            {{ $category->name }}
+                            <span @if($hasChildren) class="arrow-thingy" @endif>
+                                {{ $category->name }}
+                            </span>
                         </a>
                     </div>
                 </div>
+                @if ($hasChildren)
+                    <div class="collapse" id="{{$id}}">
+                        @foreach($category->children as $child)
+                            <div class="widget-filters__item py-0 border-0 ml-4">
+                                <div class="widget-filters__item-title">
+                                    <a style="color: white; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; display: block"
+                                       href="{{ route('category.show', $child->slug) }}">
+                                        <img src="{{ $child->imageUrl() }}" style="max-height: 2.8em; margin-right: .5rem; max-width: 2.8em;">
+                                        {{ $child->name }}
+                                    </a>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
             @endforeach
         </div>
     </div>
@@ -20,6 +48,31 @@
 </div>
 
 <style>
+    .arrow-thingy {
+        position: relative;
+        padding-right: 24px;
+    }
+
+    .arrow-thingy:before {
+        border-bottom: 1px solid #fff;
+        border-right: 1px solid #fff;
+        content: "";
+        display: block;
+        height: 5px;
+        margin-top: -3px;
+        position: absolute;
+        right: 4px;
+        top: 50%;
+        transform: rotate(45deg);
+        width: 5px;
+        transition: transform 0.2s;
+    }
+
+    [aria-expanded="true"] > .arrow-thingy:before {
+        margin-top: 0;
+        transform: rotate(225deg);
+    }
+
     .sidebar__content {
         width: 19rem;
         position: fixed;
