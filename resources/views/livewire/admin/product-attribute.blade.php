17912ed2967c22d32186add34ba684f974c554af
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-09-05 16:20:14 +0300

    fix livewire select2

--- resources/views/livewire/admin/product-attribute.blade.php
+++ resources/views/livewire/admin/product-attribute.blade.php
@@ -1,9 +1,7 @@
-<tr wire:model.live="value" x-data x-init="
-$($refs.select).select2().on('select2:select', () => {
-    $dispatch('input', $refs.select.value)
-})
+<tr wire:model.live="value" x-data
+    x-init=" $($refs.select).select2().on('select2:select', () => $dispatch('input', $refs.select.value))
 ">
-    <td>evtbu{{ $name }}</td>
+    <td>{{ $name }}</td>
     <td>
         @if($isMandatory)
             <b>Evet</b>
@@ -15,15 +13,17 @@ $($refs.select).select2().on('select2:select', () => {
         @if($isText)
             <input type="text" wire:model.live="value">
         @else
-            <select
-                class="product-attribute-selector form-select" x-ref="select" wire:ignore>
-                <option disabled>Seçiniz</option>
-                @foreach($getIterator() ?? [] as $value)
-                    <option value="{{ $getOptionValue($value) }}">
-                        {{ $getOptionText($value) }}
-                    </option>
-                @endforeach
-            </select>
+            <div wire:ignore>
+                <select
+                    class="product-attribute-selector form-select" x-ref="select" wire:ignore>
+                    <option disabled>Seçiniz</option>
+                    @foreach($getIterator() ?? [] as $value)
+                        <option value="{{ $getOptionValue($value) }}">
+                            {{ $getOptionText($value) }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
         @endif
     </td>
     <td>
