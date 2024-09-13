b01876d3c4fecd911d4e476e52b8cf7c76b5febd
lifecodeof <m.semihkoyuncu01@gmail.com>
2024-09-13 11:22:11 +0300

    remove defualt address

--- resources/views/livewire/user/add-address.blade.php
+++ resources/views/livewire/user/add-address.blade.php
@@ -23,8 +23,9 @@
                                                        name="type" value="company" wire:model.live="type"
                                                        id="default-address">
                                                 <span class="input-check__box"></span>
-                                                <span class="input-check__icon"><svg width="9px"
-                                                                                     height="7px">
+                                                <span class="input-check__icon">
+                                                    <svg width="9px"
+                                                         height="7px">
                                                         <path
                                                             d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z"/>
                                                     </svg>
@@ -42,8 +43,9 @@
                                                        name="type" value="individual" wire:model.live="type"
                                                        id="individual-address">
                                                 <span class="input-check__box"></span>
-                                                <span class="input-check__icon"><svg width="9px"
-                                                                                     height="7px">
+                                                <span class="input-check__icon">
+                                                    <svg width="9px"
+                                                         height="7px">
                                                         <path
                                                             d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z"/>
                                                     </svg>
@@ -61,21 +63,24 @@
                                     <label for="address-first-name">Ad</label>
                                     <input type="text" class="form-control" id="address-first-name" name="first_name"
                                            placeholder="Ad" value="{{ old('first_name', $address?->first_name) }}">
-                                    @error('first_name') <span class="error">{{ $message }}</span> @enderror
+                                    @error('first_name')
+                                    <span class="error">{{ $message }}</span> @enderror
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="address-last-name">Soyad</label>
                                     <input type="text" class="form-control" id="address-last-name" name="last_name"
                                            value="{{ old('last_name', $address?->last_name) }}" placeholder="Soyad">
-                                    @error('last_name') <span class="error">{{ $message }}</span> @enderror
+                                    @error('last_name')
+                                    <span class="error">{{ $message }}</span> @enderror
                                 </div>
                             </div>
                             <div class="form-row">
                                 <div class="form-group col-md-12 mb-0">
-                                    <label for="address-identity">T.C. Kimlik Numarası </label>
+                                    <label for="address-identity">T.C. Kimlik Numarası</label>
                                     <input type="text" class="form-control" id="address-identity" name="identity"
                                            value="{{ old('identity', $address?->identity) }}">
-                                    @error('identity') <span class="error">{{ $message }}</span> @enderror
+                                    @error('identity')
+                                    <span class="error">{{ $message }}</span> @enderror
                                 </div>
                             </div>
                             <div class="form-row">
@@ -83,31 +88,38 @@
                                     <label for="address-identity">Doğum tarihi</label>
                                     <input type="date" class="form-control" id="address-identity" name="birthdate"
                                            value="{{ old('birthdate', $address?->birthdate) }}">
-                                    @error('birthdate') <span class="error">{{ $message }}</span> @enderror
+                                    @error('birthdate')
+                                    <span class="error">{{ $message }}</span> @enderror
                                 </div>
                             </div>
                         @else
                             <div class="form-row">
                                 <div class="form-group col-md-6 mb-0">
-                                    <label for="address-email">Vergi Dairesi </label>
+                                    <label for="address-email">Vergi Dairesi</label>
                                     <input type="text" class="form-control" id="address-email" name="tax_office"
                                            value="{{ old('tax_office', $address?->tax_office) }}" placeholder="Avcılar">
-                                    @error('tax_office') <span class="error">{{ $message }}</span> @enderror
+                                    @error('tax_office')
+                                    <span class="error">{{ $message }}</span> @enderror
                                 </div>
                                 <div class="form-group col-md-6 mb-0">
                                     <label for="address-tax-number">Vergi No</label>
                                     <input type="text" class="form-control" id="address-tax-number" name="tax_number"
                                            value="{{ old('tax_number', $address?->tax_number) }}" placeholder="1123">
-                                    @error('tax_number') <span class="error">{{ $message }}</span> @enderror
+                                    @error('tax_number')
+                                    <span class="error">{{ $message }}</span> @enderror
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="address-company-name">Şirket Adı<span
-                                        class="text-muted">(Varsa)</span></label>
+                                <label for="address-company-name">Şirket Adı
+                                    <span
+                                        class="text-muted">(Varsa)
+                                    </span>
+                                </label>
                                 <input type="text" class="form-control" id="address-company-name" name="company_name"
                                        value="{{ old('company_name', $address?->company_name) }}"
                                        placeholder="Şirket Adı">
-                                @error('company_name') <span class="error">{{ $message }}</span> @enderror
+                                @error('company_name')
+                                <span class="error">{{ $message }}</span> @enderror
                             </div>
                         @endif
 
@@ -116,7 +128,7 @@
                             <input type="text" class="form-control" id="address-phone" name="phone"
                                    placeholder="05xx xxx xx xx" value="{{ old('phone', $address?->phone) }}">
                         </div>
-                        <livewire:city-district :city="old('city', $address?->city)" :town="old('district', $address?->district)" />
+                        <livewire:city-district :city="old('city', $address?->city)" :town="old('district', $address?->district)"/>
                         <div class="form-group">
                             <label for="address-address1">Adres</label>
                             <textarea type="text" class="form-control" name="address" id="address-address1"
@@ -124,25 +136,6 @@
                         </div>
                         <div class="form-group mb-0 pt-3 mt-3">
 
-                        </div>
-                        <div class="form-group mt-3">
-                            <div class="form-check">
-                                <span class="input-check form-check-input">
-                                    <span class="input-check__body">
-                                        <input class="input-check__input" type="checkbox"
-                                               id="default-address">
-                                        <span class="input-check__box"></span>
-                                        <span class="input-check__icon"><svg width="9px"
-                                                                             height="7px">
-                                                <path
-                                                    d="M9,1.395L3.46,7L0,3.5L1.383,2.095L3.46,4.2L7.617,0L9,1.395Z"/>
-                                            </svg>
-                                        </span>
-                                    </span>
-                                </span>
-                                <label class="form-check-label" for="default-address">Bu adresi varsayılan adres
-                                    yap</label>
-                            </div>
                         </div>
                         <div class="form-group mb-0 pt-3 mt-3">
                             <button type="submit" class="btn btn-primary">{{ $edit ? "Adresimi Düzenle":"Adres Ekle" }}</button>
