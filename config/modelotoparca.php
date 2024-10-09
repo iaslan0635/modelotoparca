f071b3f74fc61f1987385a2560070a4693024857
Turgut Efe Yüksel <turgutefeyuksel@gmail.com>
2024-10-09 13:23:15 +0300

    change proxy url

--- config/modelotoparca.php
+++ config/modelotoparca.php
@@ -3,7 +3,7 @@
 return [
     'proxy' => [
         'protocol' => 'socks5',
-        'origin' => '139.177.149.243:50101',
+        'origin' => '139.177.149.246:50101',
         'auth' => 'enproyazilim:7YhzvaWDyc',
     ],
 ];
