5ebcf40f2b23f425920f52dde751e38d815bd7ea
Semih Koyuncu <42251538+lifecodeof@users.noreply.github.com>
2024-10-16 20:52:23 +0300

    change ocp client

--- app/Services/Bots/OcpClient.php
+++ app/Services/Bots/OcpClient.php
@@ -8,19 +8,13 @@ use Illuminate\Support\Facades\Http;
 
 class OcpClient
 {
-    /**
-     * @throws OcpClientException
-     */
     public static function requestWithoutRetry(string $url): string
     {
-        return Http::get("http://141.11.109.246:5184/get-html", [
+        return Http::get("http://141.11.109.246:5184/api/Home/Get", [
             'url' => $url,
         ])->body();
     }
 
-    /**
-     * @return string
-     */
     public static function request(string $url)
     {
         $attempts = 0;
