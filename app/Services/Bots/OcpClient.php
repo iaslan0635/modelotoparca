34df369cc422815c26e9f56565d33f02fd468752
Semih Koyuncu <42251538+lifecodeof@users.noreply.github.com>
2024-10-16 11:16:23 +0300

    change ocp client

--- app/Services/Bots/OcpClient.php
+++ app/Services/Bots/OcpClient.php
@@ -3,6 +3,8 @@
 namespace App\Services\Bots;
 
 use Exception;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Facades\Http;
 
 class OcpClient
 {
@@ -11,40 +13,13 @@ class OcpClient
      */
     public static function requestWithoutRetry(string $url): string
     {
-        $curlHandle = curl_init();
-
-        $proxyUrl = config('modelotoparca.proxy.protocol').'://'.config('modelotoparca.proxy.origin');
-        curl_setopt($curlHandle, CURLOPT_PROXY, $proxyUrl);
-        curl_setopt($curlHandle, CURLOPT_PROXYUSERPWD, config('modelotoparca.proxy.auth'));
-
-        curl_setopt($curlHandle, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_3);
-        curl_setopt($curlHandle, CURLOPT_USERAGENT, 'Mozilla/5.0 (Linux; Android 12; sdk_gphone64_x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36');
-        curl_setopt($curlHandle, CURLOPT_URL, $url);
-        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curlHandle, CURLOPT_FOLLOWLOCATION, true);
-
-        $response = curl_exec($curlHandle);
-        $httpStatusCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
-        curl_close($curlHandle);
-
-        if (curl_errno($curlHandle)) {
-            throw new Exception(curl_error($curlHandle));
-        }
-        if (str_contains($response, '<title>Just a moment...</title>')) {
-            throw new Exception("Response blocked by cloudflare.\nurl: $url\nresponse: $response");
-        }
-
-        if (! ($httpStatusCode >= 200 && $httpStatusCode < 300)) {
-            throw new OcpClientException($httpStatusCode, $url, $response);
-        }
-
-        return $response;
+        return Http::get("http://141.11.109.246:5184/get-html", [
+            'url' => $url,
+        ])->body();
     }
 
     /**
      * @return string
-     *
-     * @throws OcpClientException
      */
     public static function request(string $url)
     {
@@ -53,7 +28,7 @@ class OcpClient
             try {
                 return self::requestWithoutRetry($url);
             } catch (Exception $e) {
-                $isGatewayTimeout = $e instanceof OcpClientException && $e->statusCode !== 524;
+                $isGatewayTimeout = $e instanceof ClientException && $e->getResponse()->getStatusCode() !== 524;
                 if ($isGatewayTimeout || $attempts >= 3) {
                     throw $e;
                 }
