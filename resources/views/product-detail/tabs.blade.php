aa5feb44169c63c66f4694bc7c765b68cbf1c86a
Turgut Efe Yüksel <turgutefeyuksel@gmail.com>
2024-10-23 13:56:45 +0300

    tabs orderby changed

--- resources/views/product-detail/tabs.blade.php
+++ resources/views/product-detail/tabs.blade.php
@@ -5,7 +5,19 @@
 
 <div class="product__tabs product-tabs product-tabs--layout--full">
     <ul class="product-tabs__list">
-        <li class="product-tabs__item product-tabs__item--active">
+        @if($sameProductsTab)
+            <li class="product-tabs__item product-tabs__item--active">
+                <a href="#product-same-products">Eşdeğer Ürünler</a>
+            </li>
+        @endif
+        @if($alternativesTab)
+            <li class="product-tabs__item @if(!$sameProductsTab) product-tabs__item--active @endif">
+                <a href="#product-alternative-products">
+                    Alternatif Ürünler
+                </a>
+            </li>
+        @endif
+        <li class="product-tabs__item @if(!$sameProductsTab && !$alternativesTab) product-tabs__item--active @endif">
             <a href="#product-tab-cars">
                 Uyumlu Araçlar
             </a>
@@ -19,18 +31,6 @@
         <li class="product-tabs__item">
             <a href="#reviews">Yorumlar</a>
         </li>
-        @if($sameProductsTab)
-            <li class="product-tabs__item">
-                <a href="#product-same-products">Eşdeğer Ürünler</a>
-            </li>
-        @endif
-        @if($alternativesTab)
-            <li class="product-tabs__item">
-                <a href="#product-alternative-products">
-                    Alternatif Ürünler
-                </a>
-            </li>
-        @endif
     </ul>
     <div class="product-tabs__content">
         @include('product-detail.tabs.cars')
