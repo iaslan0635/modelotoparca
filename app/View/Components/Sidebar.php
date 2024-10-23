4db9f02a54becc76e64ad951c16fb119f35263c4
Turgut Efe Yüksel <turgutefeyuksel@gmail.com>
2024-10-23 14:53:43 +0300

    whereHas and having products_count

--- app/View/Components/Sidebar.php
+++ app/View/Components/Sidebar.php
@@ -11,35 +11,24 @@ class Sidebar extends Component
     public function render()
     {
         $productCountThreshold = 0;
-
         $categories = Category::root()
             ->with(
                 'children',
                 fn($query) => $query->where("hide_in_sidebar", false)
-                    ->withCount('products') // products sayısını al
                     ->with(
                         'children',
                         fn($query) => $query->where("hide_in_sidebar", false)
-                            ->withCount('products') // iç children için de sayıyı al
                             ->with(
                                 'children',
                                 fn($query) => $query->where("hide_in_sidebar", false)
-                                    ->withCount('products') // iç iç children için de sayıyı al
+                                    ->whereHas('products', fn($q) => $q->havingRaw('COUNT(*) > ?', [$productCountThreshold]))
                             )
+                            ->whereHas('products', fn($q) => $q->havingRaw('COUNT(*) > ?', [$productCountThreshold]))
                     )
+                    ->whereHas('products', fn($q) => $q->havingRaw('COUNT(*) > ?', [$productCountThreshold]))
             )
             ->where("hide_in_sidebar", false)
             ->get();
-
-        $categories = $categories->filter(function ($category) use ($productCountThreshold) {
-            return $category->children->filter(fn($child) => $child->products_count > $productCountThreshold)->isNotEmpty();
-        });
-
-        foreach ($categories as $category) {
-            $category->children = $category->children->filter(function ($child) use ($productCountThreshold) {
-                return $child->products_count > $productCountThreshold;
-            });
-        }
 
         return view('components.sidebar', ['categories' => $categories]);
         Cache::forget("category-sidebar-view");
