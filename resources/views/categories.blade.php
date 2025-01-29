ee97e47249e370dd324779cd7ee505d81540463e
iaslan <iaslan3506@gmail.com>
2025-01-30 00:07:38 +0300

    page sayfada dar gösterim sorunu

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -9,7 +9,8 @@
             <div class="container wide-container">
                 <x-car-selection />
                 <div class="block-split__row justify-content-center row no-gutters fixed-sidebar">
-                    <div class="block-split__item block-split__item-content col-auto">
+{{--                    <div class="block-split__item block-split__item-content col-auto">--}}
+                    <div class="block-split__item block-split__item-content col-12">
                         <div class="block">
                             <div class="categories-list categories-list--layout--columns-4-full">
                                 <ul class="categories-list__body">
