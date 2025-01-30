289879a83ace90e53c7cc3fdb36246718024ab41
Turgut Efe Yüksel <turgutefeyuksel@gmail.com>
2025-01-30 17:28:58 +0300

    test 2

--- resources/views/components/product-card-image.blade.php
+++ resources/views/components/product-card-image.blade.php
@@ -48,11 +48,30 @@
 
 @script
 <script>
-    $wire.on('filtered', () => {
-        console.log("filtered")
+    Livewire.on('filtered', () => {
+        console.log("filtered");
         $('.product-gallery').each(function (i, gallery) {
             initProductGallery(gallery, "quickview", true);
-        })
+        });
     });
 </script>
 @endscript
+
+@once
+@push("scripts")
+<script>
+    onMainJsResolved(() => {
+                $('.product-gallery').each(function (i, gallery) {
+                    initProductGallery(gallery, "quickview", true);
+                })
+            })
+    Livewire.hook('morph.updated', ({ el, component }) => {
+        onMainJsResolved(() => {
+            $('.product-gallery').each(function (i, gallery) {
+                initProductGallery(gallery, "quickview", true);
+            })
+        })
+    })
+</script>
+@endpush
+@endonce
