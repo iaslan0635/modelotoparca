b1158e811479373b88855fe377fd219c871544ad
Turgut Efe Yüksel <turgutefeyuksel@gmail.com>
2025-02-01 01:09:13 +0300

    test 2

--- resources/views/components/product-list.blade.php
+++ resources/views/components/product-list.blade.php
@@ -1,16 +1,17 @@
 @props([
-    'products',
-    'car' => null,
-    'breadcrumb' => null,
-    'extraFilters',
-    'brands' => collect(),
-    'properties' => collect(),
-    'min_price' => 0,
-    'max_price',
-    'filteredProperties',
-    'category' => null,
-    'filterCategories',
-    'brandsArray' => collect(),
+'products',
+'car' => null,
+'breadcrumb' => null,
+'extraFilters',
+'brands' => collect(),
+'properties' => collect(),
+'min_price' => 0,
+'max_price',
+'filteredProperties',
+'category' => null,
+'filterCategories',
+'brandsArray' => collect(),
+'filterShow' => true,
 ])
 
 <div class="site__body">
@@ -18,13 +19,13 @@
     <div class="block-split">
         <div class="container wide-container">
             <div class="block-split__row justify-content-center row no-gutters fixed-sidebar">
-{{--                <div class="block-split__item block-split__item-content col-auto">--}}
+                {{--                <div class="block-split__item block-split__item-content col-auto">--}}
                 <div class="block-split__item block-split__item-content col-12">
                     <div class="block">
                         <div class="products-view">
-                            <x-car-selection/>
+                            <x-car-selection />
                             @if($category)
-                                <x-category-heading :category="$category" :brandIds="$brands->keys()"/>
+                            <x-category-heading :category="$category" :brandIds="$brands->keys()" />
                             @endif
                             <div class="products-view__options view-options view-options--offcanvas--mobile">
                                 <div class="view-options__body">
@@ -34,7 +35,7 @@
                                                 <path d="M7,14v-2h9v2H7z M14,7h2v2h-2V7z M12.5,6C12.8,6,13,6.2,13,6.5v3c0,0.3-0.2,0.5-0.5,0.5h-2
 	C10.2,10,10,9.8,10,9.5v-3C10,6.2,10.2,6,10.5,6H12.5z M7,2h9v2H7V2z M5.5,5h-2C3.2,5,3,4.8,3,4.5v-3C3,1.2,3.2,1,3.5,1h2
 	C5.8,1,6,1.2,6,1.5v3C6,4.8,5.8,5,5.5,5z M0,2h2v2H0V2z M9,9H0V7h9V9z M2,14H0v-2h2V14z M3.5,11h2C5.8,11,6,11.2,6,11.5v3
-	C6,14.8,5.8,15,5.5,15h-2C3.2,15,3,14.8,3,14.5v-3C3,11.2,3.2,11,3.5,11z"/>
+	C6,14.8,5.8,15,5.5,15h-2C3.2,15,3,14.8,3,14.5v-3C3,11.2,3.2,11,3.5,11z" />
                                             </svg>
                                         </span>
                                         <span class="filters-button__title">Filters</span>
@@ -43,37 +44,35 @@
                                     <div class="view-options__layout layout-switcher">
                                         <div class="layout-switcher__list">
                                             <button type="button" data-layout-id="1" class="layout-switcher__button"
-                                                    data-layout="grid"
-                                                    data-with-features="true">
+                                                data-layout="grid" data-with-features="true">
                                                 <svg width="16" height="16">
                                                     <path d="M15.2,16H9.8C9.4,16,9,15.6,9,15.2V9.8C9,9.4,9.4,9,9.8,9h5.4C15.6,9,16,9.4,16,9.8v5.4C16,15.6,15.6,16,15.2,16z M15.2,7
 	H9.8C9.4,7,9,6.6,9,6.2V0.8C9,0.4,9.4,0,9.8,0h5.4C15.6,0,16,0.4,16,0.8v5.4C16,6.6,15.6,7,15.2,7z M6.2,16H0.8
 	C0.4,16,0,15.6,0,15.2V9.8C0,9.4,0.4,9,0.8,9h5.4C6.6,9,7,9.4,7,9.8v5.4C7,15.6,6.6,16,6.2,16z M6.2,7H0.8C0.4,7,0,6.6,0,6.2V0.8
-	C0,0.4,0.4,0,0.8,0h5.4C6.6,0,7,0.4,7,0.8v5.4C7,6.6,6.6,7,6.2,7z"/>
+	C0,0.4,0.4,0,0.8,0h5.4C6.6,0,7,0.4,7,0.8v5.4C7,6.6,6.6,7,6.2,7z" />
                                                 </svg>
                                             </button>
                                             <button type="button" data-layout-id="2" class="layout-switcher__button"
-                                                    data-layout="grid"
-                                                    data-with-features="false">
+                                                data-layout="grid" data-with-features="false">
                                                 <svg width="16" height="16">
                                                     <path d="M16,0.8v14.4c0,0.4-0.4,0.8-0.8,0.8H9.8C9.4,16,9,15.6,9,15.2V0.8C9,0.4,9.4,0,9.8,0l5.4,0C15.6,0,16,0.4,16,0.8z M7,0.8
-	v14.4C7,15.6,6.6,16,6.2,16H0.8C0.4,16,0,15.6,0,15.2L0,0.8C0,0.4,0.4,0,0.8,0l5.4,0C6.6,0,7,0.4,7,0.8z"/>
+	v14.4C7,15.6,6.6,16,6.2,16H0.8C0.4,16,0,15.6,0,15.2L0,0.8C0,0.4,0.4,0,0.8,0l5.4,0C6.6,0,7,0.4,7,0.8z" />
                                                 </svg>
                                             </button>
                                             <button type="button" data-layout-id="3"
-                                                    class="layout-switcher__button layout-switcher__button--active"
-                                                    data-layout="list" data-with-features="false">
+                                                class="layout-switcher__button layout-switcher__button--active"
+                                                data-layout="list" data-with-features="false">
                                                 <svg width="16" height="16">
                                                     <path d="M15.2,16H0.8C0.4,16,0,15.6,0,15.2V9.8C0,9.4,0.4,9,0.8,9h14.4C15.6,9,16,9.4,16,9.8v5.4C16,15.6,15.6,16,15.2,16z M15.2,7
-	H0.8C0.4,7,0,6.6,0,6.2V0.8C0,0.4,0.4,0,0.8,0h14.4C15.6,0,16,0.4,16,0.8v5.4C16,6.6,15.6,7,15.2,7z"/>
+	H0.8C0.4,7,0,6.6,0,6.2V0.8C0,0.4,0.4,0,0.8,0h14.4C15.6,0,16,0.4,16,0.8v5.4C16,6.6,15.6,7,15.2,7z" />
                                                 </svg>
                                             </button>
                                             <button type="button" data-layout-id="4" class="layout-switcher__button"
-                                                    data-layout="table" data-with-features="false">
+                                                data-layout="table" data-with-features="false">
                                                 <svg width="16" height="16">
                                                     <path d="M15.2,16H0.8C0.4,16,0,15.6,0,15.2v-2.4C0,12.4,0.4,12,0.8,12h14.4c0.4,0,0.8,0.4,0.8,0.8v2.4C16,15.6,15.6,16,15.2,16z
 	 M15.2,10H0.8C0.4,10,0,9.6,0,9.2V6.8C0,6.4,0.4,6,0.8,6h14.4C15.6,6,16,6.4,16,6.8v2.4C16,9.6,15.6,10,15.2,10z M15.2,4H0.8
-	C0.4,4,0,3.6,0,3.2V0.8C0,0.4,0.4,0,0.8,0h14.4C15.6,0,16,0.4,16,0.8v2.4C16,3.6,15.6,4,15.2,4z"/>
+	C0.4,4,0,3.6,0,3.2V0.8C0,0.4,0.4,0,0.8,0h14.4C15.6,0,16,0.4,16,0.8v2.4C16,3.6,15.6,4,15.2,4z" />
                                                 </svg>
                                             </button>
                                         </div>
@@ -86,7 +85,8 @@
                                     <div class="view-options__spring"></div>
                                     <div class="view-options__select">
                                         <label for="view-option-sort">Sırala:</label>
-                                        <select id="view-option-sort" class="form-control form-control-sm" wire:model.live="sortBy">
+                                        <select id="view-option-sort" class="form-control form-control-sm"
+                                            wire:model.live="sortBy">
                                             <option value="price-asc">Yükselen Fiyat</option>
                                             <option value="price-desc">Azalan Fiyat</option>
                                             <option value="title-asc">Yükselen Başlık</option>
@@ -95,7 +95,8 @@
                                     </div>
                                     <div class="view-options__select">
                                         <label for="view-option-limit">Göster:</label>
-                                        <select id="view-option-limit" class="form-control form-control-sm" wire:model.live="pageSize">
+                                        <select id="view-option-limit" class="form-control form-control-sm"
+                                            wire:model.live="pageSize">
                                             <option value="12">12</option>
                                             <option value="24">24</option>
                                             <option value="48">48</option>
@@ -110,8 +111,8 @@
                                             :categories="$filterCategories" :$properties
                                             :price-min="isset($min_price) ? (int)$min_price : null"
                                             :price-max="isset($max_price) ? (int)$max_price : null"
-                                            :selected-category-id="$category?->id" :selected-brands="collect($brandsArray)"
-                                        />
+                                            :selected-category-id="$category?->id"
+                                            :selected-brands="collect($brandsArray)" />
                                     </div>
                                 </div>
                             </div>
@@ -120,17 +121,18 @@
                     </div>
                 </div>
             </div>
-            <x-decorated-product-carousel title="Son ziyaret edilenler" :products="\App\Packages\LatestProducts::items()" class="px-4 mt-4"/>
+            <x-decorated-product-carousel title="Son ziyaret edilenler"
+                :products="\App\Packages\LatestProducts::items()" class="px-4 mt-4" />
             <div class="block-space block-space--layout--before-footer"></div>
         </div>
     </div>
-    <x-wire-loading/>
+    <x-wire-loading />
 </div>
 
 
 @push('scripts')
-    <script>
-        // Persist layout selection
+<script>
+    // Persist layout selection
         const updateSelection = () => {
             const selectedLayoutId = Number(localStorage.getItem("selected-layout") ?? "3")
             const el = document.querySelector(`.layout-switcher__button[data-layout-id="${selectedLayoutId}"]`)
@@ -144,5 +146,5 @@
 
         updateSelection()
         $(".layout-switcher__button").click(e => localStorage.setItem("selected-layout", e.currentTarget.dataset.layoutId))
-    </script>
+</script>
 @endpush
--- resources/views/products-page.blade.php
+++ resources/views/products-page.blade.php
@@ -1,5 +1,5 @@
 @extends("layouts.master")
 @section("content")
-<x-product-list :products="$query->clone()->paginate(10)" :filter-categories="collect([])">
+<x-product-list :products="$query->clone()->paginate(10)" :filter-categories="collect([])" :filterShow="false">
 </x-product-list>
 @endsection
