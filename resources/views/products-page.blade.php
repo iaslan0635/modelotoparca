248776d26ba0ee7afdef56756a82a7faae45ee57
Turgut Efe Yüksel <turgutefeyuksel@gmail.com>
2025-02-01 01:07:38 +0300

    new test

--- resources/views/products-page.blade.php
+++ resources/views/products-page.blade.php
@@ -1,5 +1,5 @@
 @extends("layouts.master")
 @section("content")
-<x-product-list :products="$query->clone()->paginate(10)">
+<x-product-list :products="$query->clone()->paginate(10)" :filter-categories="[]">
 </x-product-list>
 @endsection
