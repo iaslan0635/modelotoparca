38af6fc2f3c9bc81614113df11a9bab0fa92478d
Turgut Efe Yüksel <turgutefeyuksel@gmail.com>
2025-02-01 01:06:41 +0300

    test 2

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -21,8 +21,6 @@ class BrandController extends Controller
         $filterCategories = $query->clone()->with('categories:id,name,slug')->get("id")->pluck('categories')->flatten()->unique('name');
         $filterCategories = ProductFilters::normalizeCategories($filterCategories);
 
-        return $filterCategories;
-
         return view('products-page', compact('query', 'filterCategories'));
     }
 
