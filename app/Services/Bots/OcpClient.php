ed525eeea78e4852d0dd06de6813238d363e9854
Turgut Efe Yüksel <turgutefeyuksel@gmail.com>
2025-04-15 15:42:41 +0300

    bug fixes

--- app/Services/Bots/OcpClient.php
+++ app/Services/Bots/OcpClient.php
@@ -8,13 +8,13 @@ use Illuminate\Support\Facades\Http;
 
 class OcpClient
 {
-    public static function requestWithoutRetry(string $url): string
+    public static function requestWithoutRetry(string $url, bool $gzipRequired = false): string
     {
             $proxy = "socks5://enproyazilim:7YhzvaWDyc@140.228.25.183:50101";
             $headers = [
                 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0',
                 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/jpeg,image/png,image/apng,*/*;q=0.8',
-                'Accept-Encoding: gzip, deflate, br',
+                'Accept-Encoding: ' . $gzipRequired ? "gzip, deflate, br":"deflate, br",
                 'Accept-Language: tr,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
                 'Cache-Control: no-cache',
                 'Referer: https://www.onlinecarparts.co.uk/spare-parts/bmw/brake-pads/5-f10-f18.html',
