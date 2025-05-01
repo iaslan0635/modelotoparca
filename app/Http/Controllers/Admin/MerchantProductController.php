0259e5591f5c1421fffbb0397f2afb87a38083bc
iaslan <iaslan3506@gmail.com>
2025-05-01 18:15:40 +0300

    pazaryerine satışa açılan ürünlerin tümünü görebilme

--- app/Http/Controllers/Admin/MerchantProductController.php
+++ app/Http/Controllers/Admin/MerchantProductController.php
@@ -9,15 +9,15 @@ class MerchantProductController extends Controller
 {
     public function index()
     {
-        $query = Product::whereHas('merchants');
+//        $query = Product::whereHas('merchants');
+        $query = Product::where('ecommerce', true);
 
         return ProductController::tableResponse($query);
     }
 
     public function onSaleIndex()
     {
-//        $query = Product::whereHas('merchants')->where('ecommerce', true);
-        $query = Product::where('ecommerce', true);
+        $query = Product::whereHas('merchants')->where('ecommerce', true);
 
         return ProductController::tableResponse($query);
     }
